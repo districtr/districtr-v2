89c601044860b99a5d01a37af399573111d80b37
Raphael Paul Laude <raphlaude@gmail.com>
2024-06-26 23:04:49 -0400

    add sidebar

---
+++ app/src/app/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import { ZoneTypeSelector } from "./Picker";
+
+export default function SidebarComponent() {
+  return (
+    <div className="h-full w-sidebar m-3 pt-3">
+      <ZoneTypeSelector />
+    </div>
+  );
+}
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme
+          accentColor="indigo"
+          grayColor="gray"
+          radius="large"
+          scaling="95%"
+        >
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
