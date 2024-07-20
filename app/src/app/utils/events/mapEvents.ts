e9d450c0a544b0b7736ac9c18df2758f838e1e9c
mariogiampieri <mariogiampieri@gmail.com>
2024-07-20 00:06:20 -0400

    lint + add `activeTool` logic

--- app/src/app/utils/events/mapEvents.ts
+++ app/src/app/utils/events/mapEvents.ts
@@ -10,77 +10,123 @@ import React from "react";
 
 import { HighlightFeature, SelectFeatures } from "./handlers";
 
-export const userMovedMouse = (e: MapLayerMouseEvent) => {};
+export const userMovedMouse = (
+  e: MapLayerMouseEvent | MapLayerTouchEvent
+) => {};
 
 /*
 MapEvent handling; these functions are called by the event listeners in the MapComponent
 */
+
+/**
+ * What happens when the map is clicked on; incomplete implementation
+ * @param e - MapLayerMouseEvent | MapLayerTouchEvent, the event object
+ * @param map - MutableRefObject<Map | null>, the maplibre map instance
+ * @param hoverFeatureIds - React.MutableRefObject<Set<string>>, used to keep track of geoids that have been hovered over
+ */
 export const handleMapClick = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {
   const mapStore = useMapStore.getState();
-  const bbox = boxAroundPoint(e, mapStore.brushSize);
+  const activeTool = mapStore.activeTool;
 
-  const selectedFeatures = map.current?.queryRenderedFeatures(bbox, {
-    layers: [BLOCK_LAYER_ID],
-  });
-  // TODO: refer to logic in reducer and original; this is a v2 test implementation
-  SelectFeatures(selectedFeatures, map, mapStore);
+  if (activeTool === "brush" || activeTool === "eraser") {
+    const bbox = boxAroundPoint(e, mapStore.brushSize);
+
+    const selectedFeatures = map.current?.queryRenderedFeatures(bbox, {
+      layers: [BLOCK_LAYER_ID],
+    });
+
+    if (activeTool === "brush") {
+      SelectFeatures(selectedFeatures, map, mapStore);
+    } else if (activeTool === "eraser") {
+      // erase features
+      // TODO: implement eraser
+    }
+  } else {
+    // tbd, for pan mode - is there an info mode on click?
+  }
 };
+
 export const handleMapMouseUp = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {};
+
 export const handleMapMouseDown = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
-) => {};
+) => {
+  const mapStore = useMapStore.getState();
+  const activeTool = mapStore.activeTool;
+  if (activeTool === "pan") {
+    return;
+  } else if (activeTool === "brush" || activeTool === "eraser") {
+    if (activeTool === "brush") {
+      /* i think we would tally all the features that are dragged over,
+       * and then on mouseup, we would assign them to the selected zone
+       **/
+      userMovedMouse(e);
+      return;
+    }
+  }
+};
+
 export const handleMapMouseEnter = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {};
+
 export const handleMapMouseOver = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {};
+
 export const handleMapMouseLeave = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {};
+
 export const handleMapMouseOut = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {};
+
 export const handleMapMouseMove = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {
-  const bbox = boxAroundPoint(e, 50);
+  const mapStore = useMapStore.getState();
+  const brushSize = mapStore.brushSize;
+  const bbox = boxAroundPoint(e, brushSize);
   const selectedFeatures = map.current?.queryRenderedFeatures(bbox, {
     layers: [BLOCK_LAYER_ID],
   });
   // TODO: refer to logic in reducer; this is a v2 test implementation
   HighlightFeature(selectedFeatures, map, hoverFeatureIds);
 };
+
 export const handleMapZoom = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
   hoverFeatureIds: React.MutableRefObject<Set<string>>
 ) => {};
+
 export const handleMapIdle = () => {};
 export const handleMapMoveEnd = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>
 ) => {};
+
 export const handleMapZoomEnd = (
   e: MapLayerMouseEvent | MapLayerTouchEvent,
   map: MutableRefObject<Map | null>,
