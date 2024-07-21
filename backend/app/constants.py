220b1eac56e71babcccb8b96e7b803f1600387c8
Raphael Paul Laude <raphlaude@gmail.com>
2024-07-21 17:58:36 -0400

    move tests to folder and add fixtures

---
+++ backend/app/constants.py
@@ -0,0 +1,4 @@
+# must match the schema created in the database, see
+# migration `966d8d72887e_add_gerrydb_schema.py`
+# DO NOT CHANGE THIS CONSTANT
+GERRY_DB_SCHEMA = "gerrydb"
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -4,22 +4,26 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session, create_engine
 
 from app.main import app, get_session
+from app.constants import GERRY_DB_SCHEMA
 from pydantic_core import MultiHostUrl
 from sqlalchemy import text
 from sqlalchemy.exc import OperationalError, ProgrammingError
 import subprocess
 import uuid
+from tests.constants import FIXTURES_PATH
+from tests.utils import string_to_bool
 
 
 client = TestClient(app)
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT")
-POSTGRES_TEST_DB = "districtr_test"
+POSTGRES_TEST_DB = os.environ.get("POSTGRES_TEST_DB", "districtr_test")
 POSTGRES_SCHEME = "postgresql+psycopg"
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
 POSTGRES_SERVER = os.environ.get("POSTGRES_SERVER", "localhost")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
+TEARDOWN_TEST_DB = string_to_bool(os.environ.get("TEARDOWN_TEST_DB", "true"))
 
 my_env = os.environ.copy()
 
@@ -69,14 +73,15 @@ def engine_fixture(request):
     subprocess.run(["alembic", "upgrade", "head"], check=True, env=my_env)
 
     def teardown():
-        # close_connections_query = f"""
-        #     SELECT pg_terminate_backend(pg_stat_activity.pid)
-        #     FROM pg_stat_activity
-        #     WHERE pg_stat_activity.datname = '{POSTGRES_TEST_DB}'
-        #     AND pid <> pg_backend_pid();
-        #     """
-        # conn.execute(text(close_connections_query))
-        # conn.execute(text(f"DROP DATABASE {POSTGRES_TEST_DB}"))
+        if TEARDOWN_TEST_DB:
+            close_connections_query = f"""
+                SELECT pg_terminate_backend(pg_stat_activity.pid)
+                FROM pg_stat_activity
+                WHERE pg_stat_activity.datname = '{POSTGRES_TEST_DB}'
+                AND pid <> pg_backend_pid();
+                """
+            conn.execute(text(close_connections_query))
+            conn.execute(text(f"DROP DATABASE {POSTGRES_TEST_DB}"))
         conn.close()
 
     request.addfinalizer(teardown)
@@ -105,6 +110,28 @@ def client_fixture(session: Session):
     app.dependency_overrides.clear()
 
 
+@pytest.fixture(scope="session", name="ks_demo_view_census_blocks")
+def ks_demo_view_census_blocks_fixture():
+    layer = "ks_demo_view_census_blocks"
+    result = subprocess.run(
+        args=[
+            "ogr2ogr",
+            "-f",
+            "PostgreSQL",
+            f"PG:host={POSTGRES_SERVER} port={POSTGRES_PORT} dbname={POSTGRES_TEST_DB} user={POSTGRES_USER} password={POSTGRES_PASSWORD}",
+            os.path.join(FIXTURES_PATH, "ks_demo_view_census_blocks.geojson"),
+            "-lco",
+            "OVERWRITE=yes",
+            "-nln",
+            f"{GERRY_DB_SCHEMA}.{layer}",  # Forced that the layer is imported into the gerrydb schema
+        ],
+    )
+
+    if result.returncode != 0:
+        print(f"ogr2ogr failed. Got {result}")
+        raise ValueError(f"ogr2ogr failed with return code {result.returncode}")
+
+
 def test_db_is_alive(client):
     response = client.get("/db_is_alive")
     assert response.status_code == 200
@@ -112,9 +139,12 @@ def test_db_is_alive(client):
 
 
 def test_new_document(client):
-    print(TEST_SQLALCHEMY_DATABASE_URI)
     response = client.post("/create_document")
     assert response.status_code == 201
     document_id = response.json().get("document_id", None)
     assert document_id is not None
     assert isinstance(uuid.UUID(document_id), uuid.UUID)
+
+
+def test_get_document_population_totals(client, ks_demo_view_census_blocks):
+    assert True
