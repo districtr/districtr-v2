9dc2ab9714a28a60cd88688b2b73555edd1f6194
bailliekova <novikova.go.zoom@gmail.com>
2024-07-21 21:10:31 -0500

    fix type on udf

--- backend/app/sql/create_assignment_partition_sql_udf.sql
+++ backend/app/sql/create_assignment_partition_sql_udf.sql
@@ -1,5 +1,5 @@
 CREATE FUNCTION create_assignment_partition_sql(document_id text)
-       RETURNS str LANGUAGE plpgsql AS $$
+       RETURNS text LANGUAGE plpgsql AS $$
 BEGIN
   RETURN format('CREATE TABLE assignments_%s PARTITION OF assignments
             VALUES IN (''%1$s'')', document_id);
