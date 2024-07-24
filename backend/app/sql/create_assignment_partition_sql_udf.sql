b11621725fb7fbc351513735b85dc7bf7125a98b
bailliekova <novikova.go.zoom@gmail.com>
2024-07-24 00:31:57 -0500

    explicit columns, create or replace

--- backend/app/sql/create_assignment_partition_sql_udf.sql
+++ backend/app/sql/create_assignment_partition_sql_udf.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION create_assignment_partition_sql(document_id text)
+CREATE OR REPLACE FUNCTION create_assignment_partition_sql(document_id text)
        RETURNS text LANGUAGE plpgsql AS $$
 BEGIN
   RETURN format('CREATE TABLE "assignments_%s" PARTITION OF assignments
--- backend/app/sql/create_document_udf.sql
+++ backend/app/sql/create_document_udf.sql
@@ -1,5 +1,11 @@
-CREATE FUNCTION create_document()
-    RETURNS text LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION create_document()
+    RETURNS TABLE (
+        document_id UUID,
+        created_at timestamp with time zone,
+        updated_at timestamp with time zone,
+        gerry_db_table varchar
+    ) 
+AS $$
 DECLARE
   doc_id uuid;
   doc_id_text text;
@@ -7,9 +13,14 @@ DECLARE
 BEGIN
   doc_id := gen_random_uuid();
   INSERT INTO document( document_id )  VALUES (doc_id);
-  doc_id_text := CAST(doc_id as text);
-  stmt := create_assignment_partition_sql(doc_id_text);
+  stmt := create_assignment_partition_sql(CAST(doc_id AS text));
   EXECUTE stmt;
-  RETURN doc_id;
-END
-$$;
+  RETURN QUERY 
+    SELECT document.document_id,
+            document.created_at,
+            document.updated_at,
+            document.gerrydb_table 
+    FROM document
+    WHERE document.document_id=doc_id;
+END;
+$$ LANGUAGE plpgsql;
