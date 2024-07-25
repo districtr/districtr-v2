591913d23462df4fc64f058566eb96eb948864ca
mariogiampieri <mariogiampieri@gmail.com>
2024-07-24 22:11:37 -0400

    hit `create_document` endpoint on map load + save uuid to store

--- app/src/app/api/apiHandlers.ts
+++ app/src/app/api/apiHandlers.ts
@@ -1,6 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import type { Map } from "maplibre-gl";
+import { useMapStore } from "@/app/store/mapStore";
+import { use } from "react";
 
 /**
  * Hook to save map data to the server, using a mutation.
@@ -9,7 +11,7 @@ import type { Map } from "maplibre-gl";
  */
 export const usePostMapData = () => {
   const mutation = useMutation({
-    mutationFn: postMapObject,
+    mutationFn: createMapObject,
     onMutate: (variables) => {
       // A mutation is about to happen, prepare for transaction
       // this id can be used on server side to rollback if needed
@@ -32,7 +34,7 @@ export const usePostMapData = () => {
         console.log("Error: ", error);
       }
       if (data) {
-        console.log("Data: ", data);
+        useMapStore.setState({ uuid: data.data });
       }
     },
   });
@@ -40,13 +42,45 @@ export const usePostMapData = () => {
   return mutation;
 };
 
+interface responseObject {
+  data: any;
+}
+
 /**
  * Save map data to the server.
  * @param mapObject - Map, the map object to save. In this case, the entire maplibre map object.
  * @returns Promise
  */
-const postMapObject = async (mapObject: Map) => {
+const postMapObject: (mapObject: Map) => Promise<responseObject> = async (
+  mapObject: Map
+) => {
   // return axios.post("/saveMap", mapObject);
   console.log("should be saving map now");
   return { data: "Map saved!" };
+};
+
+const createMapObject: (mapObject: Map) => Promise<responseObject> = async (
+  mapObject: Map
+) => {
+  try {
+    const returnObject = await axios
+      .post("http://127.0.0.1:8000/create_document") // should replace with env var
+      .then((res) => {
+        // successful roundtrip; return the document id
+        return res.data.document_id;
+      });
+    return { data: returnObject };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.message);
+      if (error.response) {
+        console.error("Response data:", error.response.data);
+        console.error("Response status:", error.response.status);
+        console.error("Response headers:", error.response.headers);
+      }
+    } else {
+      console.error("Unexpected error:", error);
+    }
+    throw error;
+  }
 };
