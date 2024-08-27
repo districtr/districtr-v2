518e7bacb416530a6f991775cf1f7775aced2e13
Raphael Paul Laude <raphlaude@gmail.com>
2024-08-26 22:59:04 -0400

    add jsdocstrings and note about defaults

--- app/src/app/components/sidebar/Layers.tsx
+++ app/src/app/components/sidebar/Layers.tsx
@@ -4,6 +4,17 @@ import { Heading, CheckboxGroup, Flex } from "@radix-ui/themes";
 import { useMapStore } from "@/app/store/mapStore";
 import { BLOCK_LAYER_ID, BLOCK_HOVER_LAYER_ID } from "../../constants/layers";
 
+/**
+ * toggleLayerVisibility
+ * This function is responsible for toggling the visibility of layers on the map.
+ * It takes a map reference and an array of layer IDs to toggle.
+ * Layers must already be added to the map and have the layout property "visibility"
+ * set to "none" or "visible". If the layout property is not set, this functions assumes
+ * the layer is not visible and will toggle visibility on.
+ *
+ * @param {MutableRefObject<maplibregl.Map>} mapRef - The map reference.
+ * @param {string[]} layerIds - An array of layer IDs to toggle.
+ */
 export function toggleLayerVisibility(
   mapRef: MutableRefObject<maplibregl.Map>,
   layerIds: string[],
@@ -21,20 +32,26 @@ export function toggleLayerVisibility(
   });
 }
 
+/** Layers
+ * This component is responsible for rendering the layers that can be toggled
+ * on and off in the map.
+ *
+ * TODO:
+ * - Support numbering for painted districts
+ * - Support tribes and communities
+ * - Actually check that counties are visible. The default checked state
+ *   is okay for now since the layers are visible by default but may not
+ *   always be the case.
+ */
 export default function Layers() {
   const { mapRef, selectedLayer } = useMapStore((state) => ({
     mapRef: state.mapRef,
     selectedLayer: state.selectedLayer,
   }));
 
-  const toggleBlockLayers = () => {
+  const toggleLayers = (layerIds: string[]) => {
     if (mapRef && !mapRef.current) return;
-    toggleLayerVisibility(mapRef, [BLOCK_LAYER_ID, BLOCK_HOVER_LAYER_ID]);
-  };
-
-  const toggleCountyBoundaries = () => {
-    if (mapRef && !mapRef.current) return;
-    toggleLayerVisibility(mapRef, ["counties_boundary", "counties_labels"]);
+    toggleLayerVisibility(mapRef, layerIds);
   };
 
   return (
@@ -49,7 +66,7 @@ export default function Layers() {
       >
         <CheckboxGroup.Item
           value="1"
-          onClick={toggleBlockLayers}
+          onClick={() => toggleLayers([BLOCK_LAYER_ID, BLOCK_HOVER_LAYER_ID])}
           disabled={selectedLayer === null}
         >
           Show painted districts
@@ -62,7 +79,10 @@ export default function Layers() {
         Boundaries
       </Heading>
       <CheckboxGroup.Root defaultValue={["1"]} name="contextualLayers">
-        <CheckboxGroup.Item value="1" onClick={toggleCountyBoundaries}>
+        <CheckboxGroup.Item
+          value="1"
+          onClick={() => toggleLayers(["counties_boundary", "counties_labels"])}
+        >
           Show county boundaries
         </CheckboxGroup.Item>
         <CheckboxGroup.Item value="2" disabled>
