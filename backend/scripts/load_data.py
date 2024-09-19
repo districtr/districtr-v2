e81db96e839819f7c2c3480ff4f580654d4559fc
nofurtherinformation <dhalpern@uchicago.edu>
2024-09-19 14:25:20 -0500

    Data script style

--- backend/scripts/load_data.py
+++ backend/scripts/load_data.py
@@ -3,10 +3,11 @@ import sqlalchemy as sa
 from os import environ
 from glob import glob
 
-# Optionally, set a data directory to load in 
-data_dir = environ.get("GPKG_DATA_DIR", "sample_data")
+# Optionally, set a data directory to load in
+DATA_DIR = environ.get("GPKG_DATA_DIR", "sample_data")
 # flag to load data, by default, will load data
-load_data = environ.get("LOAD_GERRY_DB_DATA", "true")
+LOAD_DATA = environ.get("LOAD_GERRY_DB_DATA", "true")
+
 
 def update_tile_column(engine):
     """
@@ -29,7 +30,9 @@ def update_tile_column(engine):
     with engine.connect() as connection:
         try:
             result = connection.execute(
-                sa.text("UPDATE public.gerrydbtable SET tiles_s3_path = CONCAT('tilesets/', name, '.pmtiles')")
+                sa.text(
+                    "UPDATE public.gerrydbtable SET tiles_s3_path = CONCAT('tilesets/', name, '.pmtiles')"
+                )
             )
             updated_rows = result.rowcount
             print(f"Successfully updated {updated_rows} rows in gerrydbtable.")
@@ -38,25 +41,26 @@ def update_tile_column(engine):
             print(f"Error updating gerrydbtable: {str(e)}")
             connection.rollback()
 
+
 def load_sample_data():
-  """
-  Load sample data from the specified data directory.
+    """
+    Load sample data from the specified data directory.
 
-  This function iterates through all files with a '.gpkg' extension in the
-  specified data directory, and for each file, it runs a script to load the
-  GerryDB view.
+    This function iterates through all files with a '.gpkg' extension in the
+    specified data directory, and for each file, it runs a script to load the
+    GerryDB view.
 
-  Args:
-    None
-  Returns:
-    None
-  """
-  for gpkg in glob(f"{data_dir}/*.gpkg"):
-    subprocess.run(["bash", "./scripts/load_gerrydb_view.sh", gpkg])
+    Args:
+      None
+    Returns:
+      None
+    """
+    for gpkg in glob(f"{DATA_DIR}/*.gpkg"):
+        subprocess.run(["bash", "./scripts/load_gerrydb_view.sh", gpkg])
+
 
 if __name__ == "__main__":
-  
-  if load_data == "true":
-    load_sample_data()
-    engine = sa.create_engine(environ.get("DATABASE_URL"))
-    update_tile_column(engine)
+    if LOAD_DATA == "true":
+        load_sample_data()
+        engine = sa.create_engine(environ.get("DATABASE_URL"))
+        update_tile_column(engine)
