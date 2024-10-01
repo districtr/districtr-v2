128a226a79eefbdb60a63628aa26bd2a8590586a
nofurtherinformation <dhalpern@uchicago.edu>
2024-10-01 12:53:37 -0500

    fix flex grow on map

--- app/src/app/components/Map.tsx
+++ app/src/app/components/Map.tsx
@@ -157,7 +157,7 @@ export const MapComponent: React.FC = () => {
 
   return (
     <div
-      className={`h-full relative w-full
+      className={`h-full relative w-full flex-1 md:h-screen 
     ${mapLock ? "pointer-events-none" : ""}
     `}
       ref={mapContainer}
