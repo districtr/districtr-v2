687a4ee0fd3c3bf25a279c87c0755dadbad520ce
nofurtherinformation <dhalpern@uchicago.edu>
2024-10-01 10:44:39 -0500

    refactor sidebar and page

--- app/src/app/map/page.tsx
+++ app/src/app/map/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { MapContextMenu } from "../components/ContextMenu";
 import { MapComponent } from "../components/Map";
 import SidebarComponent from "../components/sidebar/Sidebar";
+import MobileTopNav from "../components/sidebar/MobileTopNav";
 import { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Portal } from "@radix-ui/themes";
@@ -13,9 +14,10 @@ export default function Map() {
   if (queryClient) {
     return (
       <QueryClientProvider client={queryClient}>
-        <div className="h-screen w-screen flex justify-between p flex-col md:flex-row">
-          <MapComponent />
+        <div className="h-screen w-screen flex justify-between p flex-col-reverse md:flex-row-reverse">
           <SidebarComponent />
+          <MapComponent />
+          <MobileTopNav />
           <MapContextMenu />
         </div>
       </QueryClientProvider>
