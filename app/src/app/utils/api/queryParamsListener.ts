1f451fe2eb3f84cd8e3e7e297ea0a3a9bfd58496
nofurtherinformation <dhalpern@uchicago.edu>
2024-10-02 16:06:59 -0500

    clean up

--- app/src/app/utils/api/queryParamsListener.ts
+++ app/src/app/utils/api/queryParamsListener.ts
@@ -1,17 +1,18 @@
 import { updateDocumentFromId } from "./queries";
 
 export const getSearchParamsObersver = () => {
-  let previousUrl = '';
-  const observer = new MutationObserver(function(mutations) {
-    if (window.location.href !== previousUrl) {
-        previousUrl = window.location.href;
-        updateDocumentFromId.refetch()
-      }
+  let previousDocumentID = "";
+  const observer = new MutationObserver(() => {
+    const documentId = new URLSearchParams(window.location.search).get(
+      "document_id"
+    );
+    if (documentId && documentId !== previousDocumentID) {
+      previousDocumentID = documentId;
+      updateDocumentFromId.refetch();
+    }
   });
-  
-  const config = {subtree: true, childList: true};
-
+  const config = { subtree: true, childList: true };
   // start listening to changes
   observer.observe(document, config);
-  return observer
-}
+  return observer;
+};
