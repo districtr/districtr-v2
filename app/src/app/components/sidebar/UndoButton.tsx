fc50cafa868611372c0928dea1a08c21b3da320b
Nick Doiron <ndoiron@mapmeld.com>
2024-10-06 22:59:07 -0500

    visually reflect undo

--- app/src/app/components/sidebar/UndoButton.tsx
+++ app/src/app/components/sidebar/UndoButton.tsx
@@ -1,16 +1,25 @@
+import { BLOCK_SOURCE_ID } from "@/app/constants/layers";
+import { Zone } from "@/app/constants/types";
 import { useMapStore } from "@/app/store/mapStore";
 import { Button } from "@radix-ui/themes";
-
-import { Zone } from "../../constants/types";
 
 export function UndoButton() {
   const mapStore = useMapStore.getState();
 
   const handleClickUndo = () => {
+    const sourceLayer = mapStore.selectedLayer?.name;
     const lastAction = mapStore.recentZoneAssignments[mapStore.recentZoneAssignments.length - 1];
     const restoreMap: { [id: number]: Set<string> } = {};
     const nullList = new Set<string>();
     lastAction.forEach((zone, geoid) => {
+      mapStore.mapRef?.current?.setFeatureState(
+        {
+          source: BLOCK_SOURCE_ID,
+          id: geoid,
+          sourceLayer,
+        },
+        { zone },
+      );
       if (zone === null) {
         nullList.add(geoid);
       } else {
@@ -24,6 +33,7 @@ export function UndoButton() {
     for (const numericZone in Object.keys(restoreMap)) {
       mapStore.setZoneAssignments(Number(numericZone), restoreMap[numericZone], true);
     }
+    mapStore.recentZoneAssignments.pop();
   };
 
   return (
