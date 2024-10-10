1bc972cec0d6961289749f436280890bce209ff7
nofurtherinformation <dhalpern@uchicago.edu>
2024-10-10 11:11:28 -0500

    swap .union for new set
    TS and eslint not updated to standard methods

--- app/src/app/store/mapStore.ts
+++ app/src/app/store/mapStore.ts
@@ -155,8 +155,8 @@ export const useMapStore = create(
       newParent.forEach((parent) => existingParents.add(parent));
       // there may be a faster way to do this
       [newChildren].forEach(
-        (children) => (existingChildren = existingChildren.union(children))
-      );
+        (children) => existingChildren = new Set(...existingChildren, ...children)
+      )
 
       set({
         shatterIds: {
@@ -183,7 +183,7 @@ export const useMapStore = create(
       newParent.forEach((parent) => existingParents.add(parent));
       // there may be a faster way to do this
       newChildren.forEach(
-        (children) => (existingChildren = existingChildren.union(children))
+        (children) => existingChildren = new Set(...existingChildren, ...children)
       );
 
       set({
