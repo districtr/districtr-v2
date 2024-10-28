a6f15cf9453335045ec4a303237d97e32c6adb82
mariogiampieri <mariogiampieri@gmail.com>
2024-10-28 00:16:13 -0400

    clean up main + models

--- backend/app/main.py
+++ backend/app/main.py
@@ -100,18 +100,12 @@ async def create_document(
             DistrictrMap.child_layer.label("child_layer"),  # pyright: ignore
             DistrictrMap.tiles_s3_path.label("tiles_s3_path"),  # pyright: ignore
             DistrictrMap.num_districts.label("num_districts"),  # pyright: ignore
-            # get the extent from the gerrydb table for the parent layer
-            GerryDBTable.extent.label("extent"),  # pyright: ignore
+            DistrictrMap.extent.label("extent"),  # pyright: ignore
         )
         .where(Document.document_id == document_id)
         .join(
             DistrictrMap,
             Document.gerrydb_table == DistrictrMap.gerrydb_table_name,
-            isouter=True,
-        )
-        .join(
-            GerryDBTable,
-            DistrictrMap.parent_layer == GerryDBTable.name,
             isouter=True,
         )
         .limit(1)
@@ -218,8 +212,7 @@ async def get_document(document_id: str, session: Session = Depends(get_session)
             DistrictrMap.child_layer.label("child_layer"),  # pyright: ignore
             DistrictrMap.tiles_s3_path.label("tiles_s3_path"),  # pyright: ignore
             DistrictrMap.num_districts.label("num_districts"),  # pyright: ignore
-            # get the extent from the gerrydb table for the parent layer
-            GerryDBTable.extent.label("extent"),  # pyright: ignore
+            DistrictrMap.extent.label("extent"),  # pyright: ignore
         )  # pyright: ignore
         .where(
             Document.document_id == document_id
@@ -228,11 +221,6 @@ async def get_document(document_id: str, session: Session = Depends(get_session)
         .join(
             DistrictrMap,
             Document.gerrydb_table == DistrictrMap.gerrydb_table_name,
-            isouter=True,
-        )
-        .join(
-            GerryDBTable,
-            DistrictrMap.parent_layer == GerryDBTable.name,
             isouter=True,
         )
         .limit(1)
