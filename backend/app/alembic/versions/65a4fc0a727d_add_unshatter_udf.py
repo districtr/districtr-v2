96219f1e2422f80095018914aa9de056120e2bf0
nofurtherinformation <dhalpern@uchicago.edu>
2024-10-30 10:43:23 -0400

    ruff

--- backend/app/alembic/versions/65a4fc0a727d_add_unshatter_udf.py
+++ backend/app/alembic/versions/65a4fc0a727d_add_unshatter_udf.py
@@ -5,24 +5,22 @@ Revises: 5ab466c5650a
 Create Date: 2024-10-28 14:59:54.506026
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
-import app.models
-import sqlmodel.sql.sqltypes
 from pathlib import Path
 
 SQL_PATH = Path(__file__).parent.parent.parent / "sql"
 
 
-
 # revision identifiers, used by Alembic.
-revision: str = '65a4fc0a727d'
-down_revision: Union[str, None] = '167892041d95'
+revision: str = "65a4fc0a727d"
+down_revision: Union[str, None] = "167892041d95"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
+
 
 def upgrade() -> None:
     for file_name in [
@@ -31,7 +29,6 @@ def upgrade() -> None:
         with open(SQL_PATH / file_name, "r") as f:
             sql = f.read()
             op.execute(sql)
-
 
 
 def downgrade() -> None:
