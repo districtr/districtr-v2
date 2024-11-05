9c41bff5c26a91b8ad8845e51378991182fc74f2
nofurtherinformation <dhalpern@uchicago.edu>
2024-11-05 15:53:23 -0600

    SSR cleanup

--- app/src/app/constants/sources.ts
+++ app/src/app/constants/sources.ts
@@ -3,11 +3,10 @@ import {VectorSourceSpecification} from 'maplibre-gl';
 export function getBlocksSource(
   layer_subpath: string
 ): VectorSourceSpecification {
-  const zxyUrl = `${window.location.origin}/api/tiles/${encodeURIComponent(layer_subpath)}/{z}/{x}/{y}`
 
   return {
     type: "vector",
-    tiles: [zxyUrl],
+    tiles: [typeof window === 'undefined' ? '' : `${window.location.origin}/api/tiles/${encodeURIComponent(layer_subpath)}/{z}/{x}/{y}`],
     promoteId: "path",
   };
 }
