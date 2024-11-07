c770086f4cbcfd34e0ffbec2e98fd6036de97cfb
nofurtherinformation <dhalpern@uchicago.edu>
2024-11-07 11:14:21 -0600

    disable reset button if no zones have been assigned

--- app/src/app/components/sidebar/ResetMapButton.tsx
+++ app/src/app/components/sidebar/ResetMapButton.tsx
@@ -3,9 +3,10 @@ import {Button} from '@radix-ui/themes';
 
 export function ResetMapButton() {
   const handleClickResetMap = useMapStore(state => state.handleReset)
+  const noZonesAreAssigned = useMapStore(state => !state.zoneAssignments.size)
 
   return (
-    <Button onClick={handleClickResetMap} variant={'outline'} disabled>
+    <Button onClick={handleClickResetMap} variant={'outline'} disabled={noZonesAreAssigned}>
       Reset Map
     </Button>
   );
