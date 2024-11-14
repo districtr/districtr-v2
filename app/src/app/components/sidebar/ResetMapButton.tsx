05c6d238d39e9b1f381a8fe2bb0b6858309cc8fc
Dylan <dhalpern@uchicago.edu>
2024-11-14 07:11:21 -0600

    Alert dialog for map reset (#174)

--- app/src/app/components/sidebar/ResetMapButton.tsx
+++ app/src/app/components/sidebar/ResetMapButton.tsx
@@ -1,13 +1,35 @@
 import {useMapStore} from '@/app/store/mapStore';
-import {Button} from '@radix-ui/themes';
+import {AlertDialog, Button, Flex} from '@radix-ui/themes';
 
 export function ResetMapButton() {
   const handleClickResetMap = useMapStore(state => state.handleReset);
   const noZonesAreAssigned = useMapStore(state => !state.zoneAssignments.size);
 
   return (
-    <Button onClick={handleClickResetMap} variant={'outline'} disabled={noZonesAreAssigned}>
-      Reset Map
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger disabled={noZonesAreAssigned}>
+        <Button variant='outline'>Reset Map</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Reset Map</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This will reset all zone assignments and broken geographies. Resetting your
+          map cannot be undone.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" onClick={handleClickResetMap}>
+              Reset Map
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 }
