9968de21f5fc987d569b0ddeff748d48f0724e25
Anna Bailliekova <Novikova.go.zoom@gmail.com>
2024-11-18 10:11:56 -0600

    Fixes #30 Summary stats metadata endpoint (#87)
    Co-authored-by: Raphael Paul Laude <raphlaude@gmail.com>
    Co-authored-by: nofurtherinformation <dhalpern@uchicago.edu>

---
+++ backend/app/sql/summary_stats_p1.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION get_summary_stats_p1(document_id UUID)
+RETURNS TABLE (
+    zone TEXT,
+    other_pop BIGINT,
+    asian_pop BIGINT,
+    amin_pop BIGINT,
+    nhpi_pop BIGINT,
+    black_pop BIGINT,
+    white_pop BIGINT
+) AS $$
+DECLARE
+    doc_districtrmap RECORD;
+    sql_query TEXT;
+BEGIN
+    SELECT districtrmap.* INTO doc_districtrmap
+    FROM document.document
+    LEFT JOIN districtrmap
+    ON document.gerrydb_table = districtrmap.gerrydb_table_name
+    WHERE document.document_id = $1;
+
+    IF doc_districtrmap.gerrydb_table_name IS NULL THEN
+        RAISE EXCEPTION 'Table name not found for document_id: %', $1;
+    END IF;
+
+    sql_query := format('
+        SELECT
+            assignments.zone::TEXT AS zone,
+            SUM(COALESCE(blocks.other_pop, 0))::BIGINT AS other_pop,
+            SUM(COALESCE(blocks.asian_pop, 0))::BIGINT AS asian_pop,
+            SUM(COALESCE(blocks.amin_pop, 0))::BIGINT AS amin_pop,
+            SUM(COALESCE(blocks.nhpi_pop, 0))::BIGINT AS nhpi_pop,
+            SUM(COALESCE(blocks.black_pop, 0))::BIGINT AS black_pop,
+            SUM(COALESCE(blocks.white_pop, 0))::BIGINT AS white_pop
+        FROM document.assignments
+        LEFT JOIN gerrydb.%I blocks
+        ON blocks.path = assignments.geo_id
+        WHERE assignments.document_id = $1
+        GROUP BY assignments.zone
+    ', doc_districtrmap.gerrydb_table_name);
+    RETURN QUERY EXECUTE sql_query USING $1;
+END;
+$$ LANGUAGE plpgsql;
