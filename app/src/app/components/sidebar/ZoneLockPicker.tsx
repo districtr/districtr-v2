0bb2fe8cb64844279a378edbffc5b3b075dcfe0f
nofurtherinformation <dhalpern@uchicago.edu>
2024-11-21 10:48:33 -0600

    fix lock all

--- app/src/app/components/sidebar/ZoneLockPicker.tsx
+++ app/src/app/components/sidebar/ZoneLockPicker.tsx
@@ -12,7 +12,7 @@ export function ZoneLockPicker() {
   const lockedZones = useMapStore(state => state.mapOptions.lockPaintedAreas);
   const mapDocument = useMapStore(state => state.mapDocument)
   const numDistricts = mapDocument?.num_districts || 40
-  const allDistrictsNumbers = new Array(numDistricts-1).fill(null).map((_,i) => i+1)
+  const allDistrictsNumbers = new Array(numDistricts).fill(null).map((_,i) => i+1)
   const pickerValue = Array.isArray(lockedZones)
     ? lockedZones.map(f => (null === f ? 0 : f - 1))
     : lockedZones === true
