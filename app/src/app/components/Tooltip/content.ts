87efcb0159e4e212d1e6c272dbb083657b75cfd9
nofurtherinformation <dhalpern@uchicago.edu>
2024-12-09 16:02:23 -0600

    additional tooltip content

--- app/src/app/components/Tooltip/content.ts
+++ app/src/app/components/Tooltip/content.ts
@@ -1,5 +1,6 @@
 export const tooltipContent = {
   barScaling:
     'Scale population bars based on the current zone population range to work on defailed population balancing. By default, bars will show from zero to the ideal population.',
-  topToBottomDeviation: `The top-to-bottom deviation is the difference in population between the largest and smallest districts.`  
+  topToBottomDeviation: `The top-to-bottom deviation is the difference in population between the largest and smallest districts.`,
+  maxDeviation: `The maximum deviation is the largest deviation from the ideal population. You can use either a percentage of the ideal population, or a fixed number of people.`,
 } as const
