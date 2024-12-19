1514a900947814572bd3e6118ff4c610e303e430
nofurtherinformation <dhalpern@uchicago.edu>
2024-12-19 12:37:46 -0600

    Zoom to unassigned tool

---
+++ app/src/app/components/sidebar/ZoomToUnassigned.tsx
@@ -0,0 +1,58 @@
+import {useMapStore} from '@/app/store/mapStore';
+import GeometryWorker from '@/app/utils/GeometryWorker';
+import {Box, Button, Flex, Heading, Text} from '@radix-ui/themes';
+import { LngLatBoundsLike } from 'maplibre-gl';
+import React, {useEffect} from 'react';
+
+export const ZoomToUnassigned = () => {
+  const [unassignedFeatures, setUnassignedFeatures] = React.useState<GeoJSON.Feature[]>([]);
+  const mapRef = useMapStore(state => state.getMapRef());
+  const zoneAssignments = useMapStore(state => state.zoneAssignments);
+
+  const updateUnassignedFeatures = () => {
+    if (!GeometryWorker || !mapRef) return;
+    GeometryWorker.updateProps(Array.from(zoneAssignments.entries())).then(() =>
+      GeometryWorker!.getUnassignedGeometries().then(geometries => {
+        const {overall, dissolved} = geometries;
+        mapRef?.fitBounds(overall as LngLatBoundsLike);
+        if (dissolved.features.length) {
+          setUnassignedFeatures(dissolved.features);
+        } else {
+          setUnassignedFeatures([]);
+        }
+      })
+    );
+  };
+  useEffect(() => {
+    updateUnassignedFeatures();
+  }, []);
+
+  return (
+    <div>
+      {unassignedFeatures.length > 0 && (
+        <Box>
+          <Heading as="h3" size="3" my="2">
+            Found {unassignedFeatures.length} unassigned area
+            {unassignedFeatures.length > 1 ? 's' : ''}.
+          </Heading>
+          <Flex direction="row" align={'center'} gapX="2" mb="2">
+            <Text>Zoom to area{unassignedFeatures.length > 1 ? 's' : ''}:</Text>
+            {unassignedFeatures.map((feature, index) => (
+              <Button
+                key={index}
+                variant="surface"
+                color="ruby"
+                className="btn p-4"
+                onClick={() => {
+                  feature.properties?.bbox && mapRef?.fitBounds(feature.properties.bbox);
+                }}
+              >
+                {index + 1}
+              </Button>
+            ))}
+          </Flex>
+        </Box>
+      )}
+    </div>
+  );
+};
--- app/src/app/utils/GeometryWorker/geometryWorker.types.ts
+++ app/src/app/utils/GeometryWorker/geometryWorker.types.ts
@@ -56,7 +56,10 @@ export type GeometryWorkerClass = {
    * Retrieves a collection of geometries without a zone assignment.
    * @returns The collection of unassigned geometries.
    */
-  getUnassignedGeometries: () => MinGeoJSONFeature[];
+  getUnassignedGeometries: () => {
+    dissolved: GeoJSON.FeatureCollection;
+    overall: GeoJSON.BBox;
+  }
   /**
    * Retrieves the collection of geometries.
    * @returns The collection of geometries.
