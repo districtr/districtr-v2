52e03f409dff5c144960098d6c6aeb93192e0147
Nick Doiron <ndoiron@mapmeld.com>
2024-12-25 09:33:41 -0500

    oops

--- app/src/app/store/temporalStore.ts
+++ app/src/app/store/temporalStore.ts
@@ -5,7 +5,7 @@ import {useMapStore, type MapStore} from '@/app/store/mapStore';
 // Convert zundo to a React hook
 // from https://github.com/charkour/zundo?tab=readme-ov-file#for-reactive-changes-to-member-properties-of-the-temporal-object-optionally-convert-to-a-react-store-hook
 
-const useTemporalStore = <T,>(
+export const useTemporalStore = <T,>(
   selector: (state: TemporalState<Partial<MapStore>>) => T,
   equality?: (a: T, b: T) => boolean,
 ) => useStore(useMapStore.temporal, selector, equality);
