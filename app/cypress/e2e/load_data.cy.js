1e5871fa6ccaf364206d3956a90efb87edce5e89
nofurtherinformation <dhalpern@uchicago.edu>
2025-01-07 10:49:47 -0600

    add helpers

--- app/cypress/e2e/load_data.cy.js
+++ app/cypress/e2e/load_data.cy.js
@@ -1,20 +1,27 @@
+const loadData = () => {
+  cy.get('button[value="import-export"]').click()
+  // click button aria-label="Paste map data"
+  cy.get('button[aria-label="Paste map data"]').click()
+  cy.fixture('test zone assignments districtr 2025-01-07.json').then((assignments) => {
+    cy.get('textarea').paste({pastePayload: JSON.stringify(assignments),simple: true})
+    cy.get('textarea').type(" ")
+    cy.get('button[aria-label="Load map data from pasted JSON"]').click().wait(1000)
+  })
+}
+const resetMap = () => {
+  cy.get('button[aria-label="Reset map"]').click()
+  cy.get('button[aria-label="Confirm reset map"]').click().wait(1500)
+}
 
 describe('open prod site', () =>{
   it('passes', () => {
     cy.visit('http://localhost:3000/map').wait(1000)
     // cklick button value="import-export"
-    cy.get('button[value="import-export"]').click()
-    // click button aria-label="Paste map data"
-    cy.get('button[aria-label="Paste map data"]').click()
-    // find text area
-    cy.get('textarea').should('exist')
-    cy.fixture('test zone assignments districtr 2025-01-07.json').then((assignments) => {
-      cy.get('textarea').paste({pastePayload: JSON.stringify(assignments),simple: true})
-      cy.get('textarea').type(" ")
-    })
-    // find aria-label="Load map data from pasted JSON"
-    cy.get('button[aria-label="Load map data from pasted JSON"]').click().wait(1000)
-    // click on pan
-    cy.get('button[value="pan"]').click()
+    loadData()
+    cy.get('button[value="pan"]').click().wait(1000)
+    resetMap()
+    // get button aria-label ="Reset map"
+    loadData()
+    
   })
 })
