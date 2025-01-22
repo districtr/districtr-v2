fed068be2d6b1072efcf902b020be5c5f3a0fc4e
nofurtherinformation <dhalpern@uchicago.edu>
2025-01-22 15:53:51 -0600

    Add +- buttons to paint

--- app/src/app/components/Toolbar/BrushSizeSelector.tsx
+++ app/src/app/components/Toolbar/BrushSizeSelector.tsx
@@ -1,5 +1,21 @@
-import {Slider, Flex, Heading, Text} from '@radix-ui/themes';
+import {Slider, Flex, Heading, Text, IconButton} from '@radix-ui/themes';
 import {useMapStore} from '../../store/mapStore';
+import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
+import { useEffect } from 'react';
+import { debounce } from 'lodash';
+const BRUSH_MIN_SIZE = 1;
+const BRUSH_MAX_SIZE = 100;
+
+const handlePlusMinus = (change: number) => {
+  const {brushSize, setBrushSize} = useMapStore.getState();
+  let newValue = brushSize + change;
+  if (newValue > BRUSH_MAX_SIZE) {
+    newValue = BRUSH_MAX_SIZE;
+  } else if (newValue < BRUSH_MIN_SIZE) {
+    newValue = BRUSH_MIN_SIZE;
+  }
+  setBrushSize(newValue);
+}
 
 /**
  * BrushSizeSelector
@@ -15,22 +31,50 @@ export function BrushSizeSelector() {
   const setBrushSize = useMapStore(state => state.setBrushSize);
 
   const handleChangeEnd = (value: Array<number>) => {
-    console.log('the final value size is', value);
     setBrushSize(value.length ? value[0] : 0);
   };
 
+  useEffect(() => {
+    // listen for [ or - and reduce brush size
+    // listen for ] or + and increase brush size
+    const handleKeyDown = (e: KeyboardEvent) => {
+      // if alt shift or ctrl are pressed, ignore
+      if (e.altKey || e.shiftKey || e.ctrlKey) return;
+      const activeElement = document.activeElement;
+      if (activeElement instanceof HTMLInputElement || activeElement instanceof HTMLTextAreaElement)
+        return;
+
+      if (e.key === '[') {
+        handlePlusMinus(-10);
+      } else if (e.key === ']') {
+        handlePlusMinus(10);
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [])
+
   return (
-    <Flex direction="row" gap="4" maxWidth="300px" mb="3" align="center">
+    <Flex direction="row" gap="2" maxWidth="300px" mb="3" align="center">
       <Heading as="h4" size="2" weight="regular" style={{whiteSpace: 'nowrap'}}>
         Brush Size
       </Heading>
+      <IconButton variant="ghost" onClick={() => handlePlusMinus(-10)}>
+        <MinusIcon />
+      </IconButton>
       <Slider
         defaultValue={[brushSize]}
         size="2"
+        value={[brushSize]}
         onValueChange={handleChangeEnd}
-        min={1}
-        max={100}
+        min={BRUSH_MIN_SIZE}
+        max={BRUSH_MAX_SIZE}
       />
+      <IconButton variant="ghost" onClick={() => handlePlusMinus(10)}>
+        <PlusIcon />
+      </IconButton>
       <Text size="2" as="span" color="gray">
         {brushSize}
       </Text>
