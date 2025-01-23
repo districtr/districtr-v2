9c9f6b09bbd420708801ec5020fdfccacf14f2ff
nofurtherinformation <dhalpern@uchicago.edu>
2025-01-23 10:15:53 -0600

    remove old mobile nav

--- app/src/app/map/page.tsx
+++ app/src/app/map/page.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {MapContextMenu} from '../components/ContextMenu';
 import {MapComponent} from '../components/Map';
 import SidebarComponent from '../components/sidebar/Sidebar';
-import MobileTopNav from '../components/sidebar/MobileTopNav';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from '../utils/api/queryClient';
 import { ErrorNotification } from '../components/ErrorNotification';
@@ -15,7 +14,6 @@ export default function Map() {
         <div className="h-screen w-screen overflow-hidden flex justify-between p flex-col-reverse lg:flex-row-reverse landscape:flex-row-reverse">
           <SidebarComponent />
           <MapComponent />
-          <MobileTopNav />
           <MapContextMenu />
           <ErrorNotification />
         </div>
