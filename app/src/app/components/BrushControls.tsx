6b579328ad3c42d023c298ff92f7dc02665ca210
nofurtherinformation <dhalpern@uchicago.edu>
2025-01-23 11:41:43 -0600

    remove mobile color picker (for now)

--- app/src/app/components/BrushControls.tsx
+++ app/src/app/components/BrushControls.tsx
@@ -8,25 +8,14 @@ export const BrushControls = () => {
   const activeTool = useMapStore(state => state.activeTool);
 
   return (
-    <div
-      className="gap-4 lg:gap-0 landscape:gap-0
-  flex flex-row-reverse lg:flex-col landscape:flex-col
-  justify-around
-  min-w-60
-  "
-    >
+    <div className="gap-0 flex flex-col justify-around min-w-60">
       <div className="flex-grow">
         <BrushSizeSelector />
         <PaintByCounty />{' '}
       </div>
       {activeTool === 'brush' ? (
-        <div className="flex-grow-0 flex-row">
-          <span className="hidden md:block landscape:block">
-            <ZonePicker />
-          </span>
-          <span className="md:hidden landscape:hidden">
-            <MobileColorPicker />
-          </span>
+        <div className="flex-grow-0 flex-row p-0 m-0">
+          <ZonePicker />
         </div>
       ) : null}
     </div>
