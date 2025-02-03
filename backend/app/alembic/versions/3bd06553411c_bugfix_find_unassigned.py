fcc1b98f23ee68662d0a9c2c359e202ecb6fad42
nofurtherinformation <dhalpern@uchicago.edu>
2025-02-03 10:13:56 -0600

    Update 3bd06553411c_bugfix_find_unassigned.py

--- backend/app/alembic/versions/3bd06553411c_bugfix_find_unassigned.py
+++ backend/app/alembic/versions/3bd06553411c_bugfix_find_unassigned.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "3bd06553411c"
-down_revision: Union[str, None] = "c41fbcfff93e"
+down_revision: Union[str, None] = "552ac8c1defd"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
