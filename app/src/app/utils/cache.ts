7cb8f92b7514c4142ebf1e5b47af27803af0ea67
nofurtherinformation <dhalpern@uchicago.edu>
2025-02-04 16:30:37 -0600

    clean up

--- app/src/app/utils/cache.ts
+++ app/src/app/utils/cache.ts
@@ -1,10 +1,6 @@
-import {DistrictrMap, Assignment} from '@utils/api/apiHandlers';
+import {DistrictrMap, LocalAssignmentsResponse} from '@utils/api/apiHandlers';
 import {openDB, IDBPDatabase, DBSchema} from 'idb';
-import {MapStore} from '@store/mapStore';
 import {NullableZone} from '../constants/types';
-
-type DocumentID = string;
-
 interface DistrictrIDB extends DBSchema {
   lastUpdated: {
     key: string;
@@ -27,39 +23,6 @@ interface DistrictrIDB extends DBSchema {
     key: 'views';
   };
 }
-
-type MapStoreCache = {
-  zoneAssignments: MapStore['zoneAssignments'];
-  shatterIds: MapStore['shatterIds'];
-  shatterMappings: MapStore['shatterMappings'];
-};
-export const convertStateObjToObj = (state: MapStoreCache) => {
-  return {
-    zoneAssignments: Array.from(state.zoneAssignments.entries()),
-    shatterIds: {
-      children: Array.from(state.shatterIds.children),
-      parents: Array.from(state.shatterIds.parents),
-    },
-    shatterMappings: Object.entries(state.shatterMappings).map(([key, value]) => [
-      key,
-      Array.from(value),
-    ]),
-  };
-};
-
-export const hydrateStateObjFromObj = (obj: any) => {
-  return {
-    zoneAssignments: new Map(obj.zoneAssignments),
-    shatterIds: {
-      children: new Set(obj.shatterIds.children),
-      parents: new Set(obj.shatterIds.parents),
-    },
-    shatterMappings: Object.fromEntries(
-      obj.shatterMappings.map(([key, value]: [string, string[]]) => [key, new Set(value)])
-    ),
-  } as MapStoreCache;
-};
-
 class DistrictrIdbCache {
   db: IDBPDatabase<DistrictrIDB> | undefined = undefined;
   constructor() {
@@ -82,15 +45,9 @@ class DistrictrIdbCache {
   async cacheAssignments(
     document_id: string,
     updated_at: string,
-    state: {
-      zoneAssignments: MapStore['zoneAssignments'];
-      shatterIds: MapStore['shatterIds'];
-      shatterMappings: MapStore['shatterMappings'];
-    }
+    state: LocalAssignmentsResponse['data']
   ) {
-    const t0 = performance.now();
     const db = await this.init();
-
     const lastUpdatedTx = db.transaction('lastUpdated', 'readwrite');
     const lastUpdatedStore = lastUpdatedTx.objectStore('lastUpdated');
     const shatterIdsTx = db.transaction('shatterIds', 'readwrite');
@@ -99,7 +56,6 @@ class DistrictrIdbCache {
     const shatterMappingsStore = shatterMappingsTx.objectStore('shatterMappings');
     const assignmentTx = db.transaction('assignments', 'readwrite');
     const assignmentStore = assignmentTx.objectStore(`assignments`);
-
     await Promise.all([
       lastUpdatedStore.put(updated_at, document_id),
       shatterIdsStore.put(state.shatterIds, document_id),
@@ -110,37 +66,29 @@ class DistrictrIdbCache {
       shatterMappingsTx.done,
       assignmentTx.done,
     ]);
-    console.log('cached assignments in', performance.now() - t0, 'ms');
   }
 
   async getCachedAssignments(document_id: string, lastUpdatedServer: string) {
-    const t0 = performance.now();
     const db = await this.init();
-    const localLastUpdated = await db.get('lastUpdated', document_id);
-    console.log('localLastUpdated', localLastUpdated, 'lastUpdatedServer', lastUpdatedServer, document_id);
+    const lastUpdatedLocal = await db.get('lastUpdated', document_id);
     if (
-      !localLastUpdated ||
+      !lastUpdatedLocal ||
       !lastUpdatedServer ||
-      new Date(lastUpdatedServer).toISOString() > new Date(localLastUpdated).toISOString()
-    )
+      new Date(lastUpdatedServer).toISOString() > new Date(lastUpdatedLocal).toISOString()
+    ) {
       return false;
-
-      const [
-        zoneAssignments,
-        shatterIds,
-        shatterMappings,
-      ] = await Promise.all([
-        db.get('assignments', document_id),
-        db.get('shatterIds', document_id),
-        db.get('shatterMappings', document_id),
-      ]);
-      if (!zoneAssignments || !shatterIds || !shatterMappings) return false;
-      console.log("Got cached assignments in", performance.now() - t0, 'ms');
-      return {
-        zoneAssignments,
-        shatterIds,
-        shatterMappings,
-      }
+    }
+    const [zoneAssignments, shatterIds, shatterMappings] = await Promise.all([
+      db.get('assignments', document_id),
+      db.get('shatterIds', document_id),
+      db.get('shatterMappings', document_id),
+    ]);
+    if (!zoneAssignments || !shatterIds || !shatterMappings) return false;
+    return {
+      zoneAssignments,
+      shatterIds,
+      shatterMappings,
+    };
   }
 
   cacheViews = async (views: DistrictrMap[]) => {
@@ -157,34 +105,3 @@ class DistrictrIdbCache {
 }
 
 export const districtrIdbCache = new DistrictrIdbCache();
-
-type AssignmentCache = {
-  assignments: Assignment[];
-  lastUpdated: string;
-};
-
-class DistrictrLocalStorageCache {
-  mapViews: DistrictrMap[] | undefined = undefined;
-
-  constructor() {
-    const cachedViews = localStorage.getItem('districtr-map-views');
-    if (cachedViews) {
-      this.mapViews = JSON.parse(cachedViews);
-    }
-  }
-
-  getCacheAssignments(document_id: string) {
-    const assignments = localStorage.getItem(`districtr-assignments-${document_id}`);
-    if (assignments) {
-      return JSON.parse(assignments) as AssignmentCache;
-    }
-  }
-
-  cacheViews = async (views: DistrictrMap[]) => {
-    this.mapViews = views;
-    localStorage.setItem('districtr-map-views', JSON.stringify(views));
-  };
-}
-
-export const districtrLocalStorageCache =
-  typeof window === 'undefined' ? {} : new DistrictrLocalStorageCache();
