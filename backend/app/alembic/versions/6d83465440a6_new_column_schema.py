e998f71982d592925f4d48f4812ee4cd707f91de
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-10 10:09:09 -0500

    ruff

--- backend/app/alembic/versions/6d83465440a6_new_column_schema.py
+++ backend/app/alembic/versions/6d83465440a6_new_column_schema.py
@@ -5,6 +5,7 @@ Revises: 119dcf98993f
 Create Date: 2025-03-10 14:45:51.980701
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -13,8 +14,8 @@ from app.constants import SQL_DIR
 
 
 # revision identifiers, used by Alembic.
-revision: str = '6d83465440a6'
-down_revision: Union[str, None] = '119dcf98993f'
+revision: str = "6d83465440a6"
+down_revision: Union[str, None] = "119dcf98993f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -23,7 +24,7 @@ def upgrade() -> None:
     with open(f"{SQL_DIR}/available_summary_stat_udf_rev2.sql") as f:
         query = f.read()
     op.execute(sa.text(query))
-    
+
     pass
 
 
