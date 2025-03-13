74eea95ca066ebd26cc85200244db53d09f69f9d
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-13 10:18:47 -0500

    clean up interface

--- app/src/app/components/Toolbar/ColorChangeModal.tsx
+++ app/src/app/components/Toolbar/ColorChangeModal.tsx
@@ -2,28 +2,8 @@ import {useMapStore} from '@/app/store/mapStore';
 import React, {useEffect, useMemo, useState} from 'react';
 import {TwitterPicker, type ColorResult} from 'react-color';
 import {Cross2Icon} from '@radix-ui/react-icons';
-import {Box, Flex, Dialog, RadioGroup} from '@radix-ui/themes';
-import {styled} from '@stitches/react';
-
-const DialogContentContainer = styled(Dialog.Content, {
-  maxWidth: 'calc(100vw - 2rem)',
-  maxHeight: 'calc(100vh-2rem)',
-});
-
-const groupItemCSS = {
-  width: 45,
-  height: 45,
-  margin: 1.1,
-  marginBottom: 2.5,
-  cursor: 'pointer',
-  color: '#fff',
-  alignItems: 'center',
-  border: '1px solid #ccc',
-  borderRadius: '8px',
-  textShadow: '1px 1px 1px #444',
-};
-const RadioGroupItem = styled(RadioGroup.Item, groupItemCSS);
-const RadioGroupRoot = styled(RadioGroup.Root, {});
+import {Box, Flex, Dialog, RadioGroup, Text, Heading} from '@radix-ui/themes';
+import {ColorPicker} from './ColorPicker';
 
 export const ColorChangeModal: React.FC<{
   open?: boolean;
@@ -35,7 +15,7 @@ export const ColorChangeModal: React.FC<{
   const mapDocument = useMapStore(store => store.mapDocument);
   const numDistricts = mapDocument?.num_districts ?? 4;
   const [dialogOpen, setDialogOpen] = React.useState(open || false);
-  const [colorSelectIndex, setColorSelectIndex] = useState(-1);
+  const [colorSelectIndex, setColorSelectIndex] = useState(0);
 
   const filteredColors = useMemo(
     () =>
@@ -51,7 +31,6 @@ export const ColorChangeModal: React.FC<{
     let dupe = [...colorScheme];
     dupe[colorSelectIndex] = color.hex;
     setColorScheme(dupe);
-    setColorSelectIndex(-1);
   };
 
   useEffect(() => {
@@ -65,10 +44,10 @@ export const ColorChangeModal: React.FC<{
         isOpen ? setDialogOpen(isOpen) : onClose ? onClose() : setDialogOpen(isOpen)
       }
     >
-      <DialogContentContainer className="max-w-[50vw]">
-        <Flex align="center" className="mb-4">
-          <Dialog.Title className="m-0 text-xl font-bold flex-1">
-            Change District Colors
+      <Dialog.Content className="max-w-[600px] max-h-[80vw]">
+        <Flex align="start">
+          <Dialog.Title className="m-0 flex-1">
+            <Heading size="4">Change District Colors</Heading>
           </Dialog.Title>
 
           <Dialog.Close
@@ -79,31 +58,33 @@ export const ColorChangeModal: React.FC<{
           </Dialog.Close>
         </Flex>
         <Box className="max-h-[40vh] overflow-y-auto">
-          <RadioGroupRoot
-            onValueChange={(value: String) => {
-              setColorSelectIndex(Number(value));
+          <Heading size="2" className="mb-1">
+            Select a district to change its color
+          </Heading>
+          <ColorPicker
+            defaultValue={0}
+            value={colorSelectIndex}
+            onValueChange={(value: number) => {
+              setColorSelectIndex(value);
             }}
-          >
-            <Flex direction="row" wrap="wrap" style={{paddingBottom: '15rem'}}>
-              {!!mapDocument &&
-                colorScheme.slice(0, numDistricts).map((color, i) => (
-                  <Flex direction="column" key={i}>
-                    <RadioGroupItem style={{backgroundColor: color}} value={String(i)}>
-                      {i + 1}
-                    </RadioGroupItem>
-                    <div style={{display: i === colorSelectIndex ? 'block' : 'none', width: 0}}>
-                      <TwitterPicker
-                        color={color}
-                        colors={filteredColors}
-                        onChangeComplete={color => handleColorPick(i, color)}
-                      />
-                    </div>
-                  </Flex>
-                ))}
+          />
+          {colorSelectIndex !== undefined && (
+            <Flex direction="column" gapY="2" mt="4">
+              <hr />
+              <Heading size="2">
+                Choose a color for <u>District {colorSelectIndex + 1}</u>
+              </Heading>
+              <TwitterPicker
+                color={colorScheme[colorSelectIndex]}
+                colors={filteredColors}
+                onChangeComplete={color => handleColorPick(colorSelectIndex, color)}
+                triangle="hide"
+                className="!shadow-none !w-full"
+              />
             </Flex>
-          </RadioGroupRoot>
+          )}
         </Box>
-      </DialogContentContainer>
+      </Dialog.Content>
     </Dialog.Root>
   );
 };
