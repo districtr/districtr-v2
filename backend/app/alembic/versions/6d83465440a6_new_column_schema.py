b94666913b56859c6f386cccdd232204e394a079
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-14 13:16:01 -0500

    Update 6d83465440a6_new_column_schema.py

--- backend/app/alembic/versions/6d83465440a6_new_column_schema.py
+++ backend/app/alembic/versions/6d83465440a6_new_column_schema.py
@@ -1,7 +1,7 @@
 """new column schema
 
 Revision ID: 6d83465440a6
-Revises: 4b0aec5f8350
+Revises: e9435b616749
 Create Date: 2025-03-10 14:45:51.980701
 
 """
@@ -15,7 +15,7 @@ from app.constants import SQL_DIR
 
 # revision identifiers, used by Alembic.
 revision: str = "6d83465440a6"
-down_revision: Union[str, None] = "4b0aec5f8350"
+down_revision: Union[str, None] = "e9435b616749"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
