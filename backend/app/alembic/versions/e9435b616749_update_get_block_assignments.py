07f94e989be466ddc33e8649b1a2ffa8742f7821
Raphael Laude <raphlaude@gmail.com>
2025-03-13 20:13:04 -0400

    update revision

--- backend/app/alembic/versions/e9435b616749_update_get_block_assignments.py
+++ backend/app/alembic/versions/e9435b616749_update_get_block_assignments.py
@@ -15,7 +15,7 @@ from app.core.config import settings
 
 # revision identifiers, used by Alembic.
 revision: str = "e9435b616749"
-down_revision: Union[str, None] = "119dcf98993f"
+down_revision: Union[str, None] = "4b0aec5f8350"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -24,16 +24,16 @@ SQL_DIR = settings.SQL_DIR
 
 
 def upgrade() -> None:
-    for udf in [
-        "get_block_assignments",
-        "get_block_assignments_geo",
-        "get_block_zone_assignments",
-        "get_block_zone_assignments_geo",
+    for udf, arg_types in [
+        ("get_block_assignments", "UUID"),
+        ("get_block_assignments_geo", "UUID"),
+        ("get_block_zone_assignments", "UUID INTEGER[]"),
+        ("get_block_zone_assignments_geo", "UUID INTEGER[]"),
     ]:
         with open(f"{SQL_DIR}/{udf}.sql", "r") as f:
             sql = f.read()
         # Need to drop the function before creating it again because the function signature has changed
-        op.execute(sa.text(f"DROP FUNCTION IF EXISTS {udf};"))
+        op.execute(sa.text(f"DROP FUNCTION IF EXISTS {udf}({arg_types});"))
         op.execute(sa.text(sql))
 
 
