f922b8e5dad857a01211aebd01e66317ea838d78
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-25 09:25:54 -0500

    formatting

--- backend/app/alembic/versions/8b06a98951aa_add_cms_content_table.py
+++ backend/app/alembic/versions/8b06a98951aa_add_cms_content_table.py
@@ -5,6 +5,7 @@ Revises: 518ab28c5fd6
 Create Date: 2025-03-25 14:07:52.761319
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -14,8 +15,8 @@ from app.models import UUIDType
 from sqlmodel.sql.sqltypes import AutoString
 
 # revision identifiers, used by Alembic.
-revision: str = '8b06a98951aa'
-down_revision: Union[str, None] = '518ab28c5fd6'
+revision: str = "8b06a98951aa"
+down_revision: Union[str, None] = "518ab28c5fd6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -44,12 +45,16 @@ def upgrade() -> None:
         sa.Column("published_content", JSONB, nullable=True),
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("slug", "language", name="slug_language_unique"),
-        sa.ForeignKeyConstraint(["districtr_map_slug"], ["districtrmap.districtr_map_slug"]),
+        sa.ForeignKeyConstraint(
+            ["districtr_map_slug"], ["districtrmap.districtr_map_slug"]
+        ),
     )
-    
+
     # Create indices for faster lookups
     op.create_index("idx_cms_content_slug", "cms_content", ["slug"])
-    op.create_index("idx_cms_content_districtr_map_slug", "cms_content", ["districtr_map_slug"])
+    op.create_index(
+        "idx_cms_content_districtr_map_slug", "cms_content", ["districtr_map_slug"]
+    )
     op.create_index("idx_cms_content_language", "cms_content", ["language"])
 
 
@@ -58,6 +63,6 @@ def downgrade() -> None:
     op.drop_index("idx_cms_content_language")
     op.drop_index("idx_cms_content_districtr_map_slug")
     op.drop_index("idx_cms_content_slug")
-    
+
     # Then drop the table
     op.drop_table("cms_content")
