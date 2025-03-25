8cf590a2ef93902b5fa3b269613ac43a9ee267b1
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-25 13:17:59 -0500

    Update page.tsx

--- app/src/app/(static)/tags/page.tsx
+++ app/src/app/(static)/tags/page.tsx
@@ -3,34 +3,31 @@ import {onlyUniqueProperty} from '@/app/utils/arrays';
 import {Box, Card, Flex, Heading, Link} from '@radix-ui/themes';
 import NextLink from 'next/link';
 
-export default async function TagsPage() {
-  try {
-    const cmsContent = await listCMSContent('tags');
-    const entries = cmsContent
-      .filter(content => content.published_content)
-      .filter(onlyUniqueProperty('slug'));
+export const dynamic = 'force-dynamic';
 
-    return (
-      <Flex direction={'column'} className="max-w-screen-xl mx-auto py-4">
-        <Heading as="h1" size="6" mb="4">
-          Problems
-        </Heading>
-        <Box>
-          {entries.map(content => (
-            <Card key={content.slug}>
-              <Heading as="h3" size="4">
-                {content.published_content!.title}
-              </Heading>
-              <NextLink href={`/tag/${content.slug}`} passHref legacyBehavior>
-                <Link>Go to tag</Link>
-              </NextLink>
-            </Card>
-          ))}
-        </Box>
-      </Flex>
-    );
-  } catch (error) {
-    console.error('Error getting tags:', error);
-    return <div>Error getting tags</div>;
-  }
+export default async function TagsPage() {
+  const cmsContent = await listCMSContent('tags');
+  const entries = cmsContent
+    .filter(content => content.published_content)
+    .filter(onlyUniqueProperty('slug'));
+
+  return (
+    <Flex direction={'column'} className="max-w-screen-xl mx-auto py-4">
+      <Heading as="h1" size="6" mb="4">
+        Problems
+      </Heading>
+      <Box>
+        {entries.map(content => (
+          <Card key={content.slug}>
+            <Heading as="h3" size="4">
+              {content.published_content!.title}
+            </Heading>
+            <NextLink href={`/tag/${content.slug}`} passHref legacyBehavior>
+              <Link>Go to tag</Link>
+            </NextLink>
+          </Card>
+        ))}
+      </Box>
+    </Flex>
+  );
 }
