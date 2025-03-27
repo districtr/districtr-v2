c36ab322e49ca7ed672c0efa5a993bc62a1b0496
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-27 13:35:41 -0500

    fix slug / places pages

--- app/src/app/(static)/place/[slug]/page.tsx
+++ app/src/app/(static)/place/[slug]/page.tsx
@@ -1,21 +1,30 @@
 import {LanguagePicker} from '@/app/components/LanguagePicker/LanguagePicker';
 import RichTextPreview from '@/app/components/RichTextPreview';
 import {CreateButton} from '@/app/components/Static/Interactions/CreateButton';
+import {getAvailableDistrictrMaps} from '@/app/utils/api/apiHandlers/getAvailableDistrictrMaps';
 import {getCMSContent} from '@/app/utils/api/cms';
-import {Box, Flex, Heading, Link} from '@radix-ui/themes';
+import {Box, Flex, Grid, Heading, Link} from '@radix-ui/themes';
 import {cookies} from 'next/headers';
 
 export default async function Page({params}: {params: Promise<{slug: string}>}) {
   const [{slug}, userCookies] = await Promise.all([params, cookies()]);
   const language = userCookies.get('language')?.value ?? 'en';
-  const cmsData = await getCMSContent(slug, language, 'places');
+  const [cmsData, maps] = await Promise.all([
+    getCMSContent(slug, language, 'places'),
+    getAvailableDistrictrMaps(),
+  ]);
 
-  if (!cmsData.content.published_content) {
+  if (!cmsData?.content?.published_content) {
     return <div>Content not found</div>;
   }
+
+  const availableMaps =
+    cmsData.content.districtr_map_slugs &&
+    maps.filter(m => cmsData.content.districtr_map_slugs!.includes(m.districtr_map_slug));
+    
   return (
-    <Flex direction="column">
-      <Flex direction="column" className="max-w-screen-lg mx-auto" py="4">
+    <Flex direction="column" width="100%" className="max-w-screen-lg mx-auto py-4">
+      <Flex direction="column">
         <Heading as="h1" size="6" mb="4">
           {cmsData.content.published_content.title}
         </Heading>
@@ -24,21 +33,26 @@ export default async function Page({params}: {params: Promise<{slug: string}>}) 
           availableLanguages={cmsData.available_languages}
         />
         <RichTextPreview content={cmsData.content.published_content.body} className="my-4" />
-        {/* @ts-ignore */}
-        {Boolean(cmsData.content.districtr_map_slugs) && (
-          <>
-            {/* @ts-ignore */}
-            {cmsData.content.districtr_map_slugs.map((slug: string, idx: number) => (
-              <CreateButton
-                key={idx}
-                view={{
-                  districtr_map_slug: slug,
-                  name: slug,
-                }}
-              />
-            ))}
-          </>
-        )}
+        <Grid 
+        gap="2"
+        columns={{
+          initial: '1',
+          md: '2',
+          lg: '4'
+        }}>
+          {Boolean(availableMaps) && (
+            <>
+              {availableMaps!.map((view, i) => (
+                <CreateButton
+                  key={i}
+                  view={{
+                    ...view,
+                  }}
+                />
+              ))}
+            </>
+          )}
+        </Grid>
       </Flex>
     </Flex>
   );
--- app/src/app/(static)/tag/[slug]/page.tsx
+++ app/src/app/(static)/tag/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import {LanguagePicker} from '@/app/components/LanguagePicker/LanguagePicker';
 import RichTextPreview from '@/app/components/RichTextPreview';
 import {CreateButton} from '@/app/components/Static/Interactions/CreateButton';
+import {getAvailableDistrictrMaps} from '@/app/utils/api/apiHandlers/getAvailableDistrictrMaps';
 import {getCMSContent} from '@/app/utils/api/cms';
 import {Flex, Heading} from '@radix-ui/themes';
 import {cookies} from 'next/headers';
@@ -8,11 +9,18 @@ import {cookies} from 'next/headers';
 export default async function Page({params}: {params: Promise<{slug: string}>}) {
   const [{slug}, userCookies] = await Promise.all([params, cookies()]);
   const language = userCookies.get('language')?.value ?? 'en';
-  const cmsData = await getCMSContent(slug, language, 'tags');
+  const [cmsData, maps] = await Promise.all([
+    getCMSContent(slug, language, 'tags'),
+    getAvailableDistrictrMaps(),
+  ]);
 
-  if (!cmsData.content.published_content) {
+  if (!cmsData?.content?.published_content) {
     return <div>Content not found</div>;
   }
+  const selectedMap =
+    cmsData.content.districtr_map_slug &&
+    maps.find(m => m.districtr_map_slug === cmsData.content.districtr_map_slug);
+
   return (
     <Flex direction="column" className="max-w-screen-lg mx-auto" py="4">
       <Heading as="h1" size="6" mb="4">
@@ -23,12 +31,10 @@ export default async function Page({params}: {params: Promise<{slug: string}>}) 
         availableLanguages={cmsData.available_languages}
       />
       <RichTextPreview content={cmsData.content.published_content.body} className="my-4" />
-      {/* @ts-ignore */}
-      {Boolean(cmsData.content.districtr_map_slug) && (
+      {Boolean(selectedMap) && (
         <CreateButton
           view={{
-            // @ts-ignore
-            districtr_map_slug: cmsData.content.districtr_map_slug,
+            ...selectedMap,
             name: 'Create Plan',
           }}
         />
