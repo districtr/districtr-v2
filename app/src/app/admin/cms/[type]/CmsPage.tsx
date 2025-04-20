33c5afcc51b18d9a51b21981ec54bee9635bdd16
Raphael Laude <raphlaude@gmail.com>
2025-04-20 14:09:38 -0400

    blocked loading when there's no content

--- app/src/app/admin/cms/[type]/CmsPage.tsx
+++ app/src/app/admin/cms/[type]/CmsPage.tsx
@@ -2,7 +2,7 @@
 
 import React, {useEffect} from 'react';
 import {CmsContentTypes} from '@/app/utils/api/cms';
-import {Blockquote, Box, Heading, Flex} from '@radix-ui/themes';
+import {Blockquote, Heading, Flex} from '@radix-ui/themes';
 // Use dynamic import for RichTextEditor to avoid SSR issues
 import {ContentPreviewModal} from '@/app/components/Cms/ContentPreviewModal';
 import {useCmsFormStore} from '@/app/store/cmsFormStore';
@@ -15,7 +15,6 @@ export const CMSAdminPage: React.FC<{
   const error = useCmsFormStore(state => state.error);
   const success = useCmsFormStore(state => state.success);
   const loadData = useCmsFormStore(state => state.loadData);
-  const isLoaded = useCmsFormStore(state => state.maps.length);
 
   useEffect(() => {
     loadData(contentType);
@@ -28,17 +27,9 @@ export const CMSAdminPage: React.FC<{
       </Heading>
       {error && <Blockquote color="red">{error}</Blockquote>}
       {success && <Blockquote color="green">{success}</Blockquote>}
-      {!isLoaded ? (
-        <Flex justify="center" align="center">
-          <Box>Loading...</Box>
-        </Flex>
-      ) : (
-        <>
-          <ContentEditor />
-          <ContentList />
-          <ContentPreviewModal />
-        </>
-      )}
+      <ContentEditor />
+      <ContentList />
+      <ContentPreviewModal />
     </Flex>
   );
 };
