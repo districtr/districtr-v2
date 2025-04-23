33f025e82d6a73795ab3fbc87ec7877a0a645242
nofurtherinformation <dhalpern@uchicago.edu>
2025-04-23 16:02:22 -0500

    clean up lingering references

--- backend/management/load_data.py
+++ backend/management/load_data.py
@@ -8,7 +8,6 @@ from app.utils import (
     create_shatterable_gerrydb_view,
     get_local_or_s3_path,
     add_extent_to_districtrmap,
-    add_available_summary_stats_to_districtrmap,
     create_spatial_index,
 )
 from app.main import get_session
@@ -289,11 +288,6 @@ def load_sample_data(
 
         logger.info(f"Adding extent to districtr map with UUID {u}")
         add_extent_to_districtrmap(session=session, districtr_map_uuid=u)
-
-        logger.info(f"Adding available summary stats to districtr map with UUID {u}")
-        _ = add_available_summary_stats_to_districtrmap(
-            session=session, districtr_map_uuid=u
-        )
 
         if view.child_layer is not None:
             _create_parent_child_edges(session=session, districtr_map_uuid=str(u))
