da332a6dc05655d2f37f2b7f5c1d5f54327256cd
mariogiampieri <mariogiampieri@gmail.com>
2025-04-26 17:29:24 -0400

    add udf + migration

---
+++ backend/app/alembic/versions/25416f500a9b_update_metadata_draft_status.py
@@ -0,0 +1,31 @@
+"""update metadata draft status
+
+Revision ID: 25416f500a9b
+Revises: 8b06a98951aa
+Create Date: 2025-04-26 17:27:04.406473
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+from pathlib import Path
+from app.constants import SQL_DIR
+
+# revision identifiers, used by Alembic.
+revision: str = "25416f500a9b"
+down_revision: Union[str, None] = "8b06a98951aa"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with Path(SQL_DIR, "update_metadata_draft_status.sql").open() as f:
+        sql = f.read()
+        op.execute(sql)
+    pass
+
+
+def downgrade() -> None:
+    op.execute("DROP FUNCTION IF EXISTS update_metadata_draft_status")
+    pass
