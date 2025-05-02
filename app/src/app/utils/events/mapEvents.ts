824c103a2d835dab73ff0df3f3d0984d21448455
nofurtherinformation <dhalpern@uchicago.edu>
2025-05-02 17:04:22 -0500

    clean up

--- app/src/app/utils/events/mapEvents.ts
+++ app/src/app/utils/events/mapEvents.ts
@@ -6,10 +6,9 @@ import type {
   MapLayerMouseEvent,
   MapLayerTouchEvent,
   MapGeoJSONFeature,
-  MapStyleDataEvent,
   MapSourceDataEvent,
 } from 'maplibre-gl';
-import type {MapRef as MapLibreMap, MapRef, ViewStateChangeEvent} from 'react-map-gl/maplibre';
+import type {MapEvent, MapRef as MapLibreMap, MapRef, ViewStateChangeEvent} from 'react-map-gl/maplibre';
 import {useMapStore} from '@/app/store/mapStore';
 import {
   BLOCK_HOVER_LAYER_ID,
@@ -22,7 +21,6 @@ import {ActiveTool} from '@/app/constants/types';
 import {throttle} from 'lodash';
 import {useTooltipStore} from '@/app/store/tooltipStore';
 import {useHoverStore} from '@/app/store/hoverFeatures';
-import {getFeatureUnderCursor} from '../helpers';
 
 export const EMPTY_FEATURE_ARRAY: MapGeoJSONFeature[] = [];
 /*
@@ -185,8 +183,8 @@ export const handleMapMouseMove = throttle((e: MapLayerMouseEvent | MapLayerTouc
 
 export const handleMapZoom = (e: ViewStateChangeEvent) => {};
 
-export const handleMapIdle = (e: ViewStateChangeEvent) => {
-  const currentZoom = e.viewState.zoom;
+export const handleMapIdle = (e: MapEvent) => {
+  const currentZoom = e.target.getZoom();
   if (GeometryWorker) {
     GeometryWorker.setMaxParentZoom(currentZoom);
   }
