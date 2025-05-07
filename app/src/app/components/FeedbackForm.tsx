ab471c6f58c3da12520ab39715238d21a82b0538
nofurtherinformation <dhalpern@uchicago.edu>
2025-05-07 16:59:14 -0500

    Add copy indicator

--- app/src/app/components/FeedbackForm.tsx
+++ app/src/app/components/FeedbackForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 import {CopyIcon, Cross1Icon, Cross2Icon} from '@radix-ui/react-icons';
-import {Box, Button, Dialog, Flex, Heading, IconButton, Text} from '@radix-ui/themes';
+import {Box, Button, Dialog, Flex, Heading, IconButton, Text, Tooltip} from '@radix-ui/themes';
 import React, {useEffect, useState} from 'react';
 
 export const FeedbackForm: React.FC = () => {
@@ -8,7 +8,9 @@ export const FeedbackForm: React.FC = () => {
   const [hovering, setHovering] = useState(false);
   const [hidden, setHidden] = useState(false);
   const [formUrl, setFormUrl] = useState<string | undefined>(undefined);
-
+  const [copyIndicator, setCopyIndicator] = useState(false);
+  // To get ENV after build time, we need to have some realtime server endpoint with env
+  // This allows us to modify this without having to rebuild the app
   useEffect(() => {
     fetch('/api/env')
       .then(res => (res.ok ? res.json() : Promise.reject(res)))
@@ -22,6 +24,8 @@ export const FeedbackForm: React.FC = () => {
   const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
   const copyUrlToClipboard = () => {
     navigator.clipboard.writeText(currentUrl);
+    setCopyIndicator(true);
+    setTimeout(() => setCopyIndicator(false), 4000);
   };
 
   return (
@@ -60,9 +64,16 @@ export const FeedbackForm: React.FC = () => {
           <Text>Please include your current URL in your feedback:</Text>
           <Flex direction="row" gap="2">
             <Text className="font-bold font-mono p-1 bg-gray-200 flex-1">{currentUrl}</Text>
-            <IconButton size="2" onClick={copyUrlToClipboard}>
-              <CopyIcon />
-            </IconButton>
+            <Tooltip content={copyIndicator ? 'Copied to clipboard' : 'Copy URL'}>
+              <IconButton
+                size="2"
+                onClick={copyUrlToClipboard}
+                color={copyIndicator ? 'green' : 'blue'}
+                className="!transition-colors !duration-500"
+              >
+                <CopyIcon />
+              </IconButton>
+            </Tooltip>
           </Flex>
           <Box className="w-full h-auto flex-1">
             <iframe src={formUrl} className="w-full h-[80vh]" />
