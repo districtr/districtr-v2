8b3d06a9a01b2253650fa690e186758aa8b26996
nofurtherinformation <dhalpern@uchicago.edu>
2025-05-09 14:04:50 -0500

    fix alembic versions

--- backend/app/alembic/versions/25416f500a9b_update_metadata_draft_status.py
+++ backend/app/alembic/versions/25416f500a9b_update_metadata_draft_status.py
@@ -1,7 +1,7 @@
 """update metadata draft status
 
 Revision ID: 25416f500a9b
-Revises: 8b06a98951aa
+Revises: ea4bc886a999
 Create Date: 2025-04-26 17:27:04.406473
 
 """
@@ -14,7 +14,7 @@ from app.constants import SQL_DIR
 
 # revision identifiers, used by Alembic.
 revision: str = "25416f500a9b"
-down_revision: Union[str, None] = "8b06a98951aa"
+down_revision: Union[str, None] = "ea4bc886a999"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- backend/app/alembic/versions/2f8e63322e06_merge_stats_metadata.py
+++ backend/app/alembic/versions/2f8e63322e06_merge_stats_metadata.py
@@ -1,7 +1,7 @@
 """merges update_stats and metadata revisions
 
 Revision ID: 2f8e63322e06
-Revises: 25416f500a9b, d38d0f766dc5
+Revises: 25416f500a9b
 Create Date: 2025-05-02 08:12:07.908662
 
 """
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
 revision: str = "2f8e63322e06"
-down_revision: Union[str, None] = ("25416f500a9b", "d38d0f766dc5")
+down_revision: Union[str, None] = "25416f500a9b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
