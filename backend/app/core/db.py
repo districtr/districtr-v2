466002987c3ad16162e2829a6577d61e3be4e7e2
Raphael Laude <raphlaude@gmail.com>
2025-05-10 23:09:15 -0400

    make sure sessions are cleaned up

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -2,14 +2,12 @@ from sqlmodel import create_engine, Session
 
 from app.core.config import settings
 
-echo = True
-
-if settings.ENVIRONMENT == "production":
-    echo = False
-
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=echo)
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=settings.ECHO_DB)
 
 
 def get_session():
     with Session(engine) as session:
-        yield session
+        try:
+            yield session
+        finally:
+            session.close()
