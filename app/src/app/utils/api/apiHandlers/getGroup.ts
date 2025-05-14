e4f19d22856bb85bc9b08847c5789f87e87c8505
Dylan <dhalpern@uchicago.edu>
2025-05-14 12:40:39 -0500

    Group page fix (#385)

--- app/src/app/utils/api/apiHandlers/getGroup.ts
+++ app/src/app/utils/api/apiHandlers/getGroup.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import {MapGroup} from './types';
+import {API_URL} from '../constants';
 
 export const getGroup = async (group_slug: string): Promise<MapGroup> => {
-  return await axios.get(`http://backend:8000/api/group/${group_slug}`).then(res => {
+  return await axios.get(`${API_URL}/api/group/${group_slug}`).then(res => {
     return res.data;
   });
 };
