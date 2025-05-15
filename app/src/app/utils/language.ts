6c50848aac09d09f90494ad2270a003b7bf9777a
nofurtherinformation <dhalpern@uchicago.edu>
2025-05-15 14:09:28 -0500

    types and defaults

--- app/src/app/utils/language.ts
+++ app/src/app/utils/language.ts
@@ -1,3 +1,5 @@
+import {DocumentMetadata} from './api/apiHandlers/types';
+
 export const LANG_MAPPING = {
   en: 'English',
   es: 'Spanish',
@@ -10,3 +12,12 @@ export const LANG_MAPPING = {
 } as const;
 
 export const MAX_TITLE_LENGTH = 36;
+
+export const DEFAULT_MAP_METADATA: DocumentMetadata = {
+  name: null,
+  group: null,
+  tags: null,
+  description: null,
+  eventId: null,
+  draft_status: 'scratch_work',
+};
