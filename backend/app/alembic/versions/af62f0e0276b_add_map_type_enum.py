13a5870bc700bd05458d5c809f7f9046a939967a
Nick Doiron <ndoiron@mapmeld.com>
2025-05-18 20:56:40 -0500

    add map type enum

---
+++ backend/app/alembic/versions/af62f0e0276b_add_map_type_enum.py
@@ -0,0 +1,35 @@
+"""Add map type enum
+
+Revision ID: af62f0e0276b
+Revises: 25416f500a9b
+Create Date: 2025-05-19 01:50:18.044942
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision: str = "af62f0e0276b"
+down_revision: Union[str, None] = "25416f500a9b"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+maptype_enum = postgresql.ENUM("default", "local", name="maptype", create_type=False)
+
+
+def upgrade() -> None:
+    maptype_enum.create(op.get_bind(), checkfirst=True)
+    op.add_column(
+        "districtrmap",
+        sa.Column("map_type", maptype_enum, nullable=False, server_default="default"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("mytable", "map_type")
+    maptype_enum.drop(op.get_bind(), checkfirst=True)
