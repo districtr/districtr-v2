c5827c4ab23aad10a2aafc43d2fcb92f37d6d880
nofurtherinformation <dhalpern@uchicago.edu>
2025-07-23 16:19:29 -0500

    Move public ID to token table and backfill

--- backend/app/alembic/versions/e1ba7028696f_add_public_id_to_districtrmap.py
+++ backend/app/alembic/versions/e1ba7028696f_add_public_id_to_districtrmap.py
@@ -11,7 +11,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = "e1ba7028696f"
 down_revision: Union[str, None] = "545e708aeb30"
@@ -21,15 +20,28 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.add_column(
-        "document",
+        "map_document_token",
         sa.Column("public_id", sa.Integer(), nullable=True),
         schema="document",
     )
     op.create_unique_constraint(
-        "uq_document_public_id", "document", ["public_id"], schema="document"
+        "uq_document_public_id", "map_document_token", ["public_id"], schema="document"
+    )
+    # update public_id to be row number
+    op.execute(
+        sa.text("""
+            WITH numbered_tokens AS (
+                SELECT token_id, row_number() OVER (ORDER BY token_id) AS rn
+                FROM document.map_document_token
+            )
+            UPDATE document.map_document_token AS t
+            SET public_id = n.rn
+            FROM numbered_tokens AS n
+            WHERE t.token_id = n.token_id;
+        """),
     )
 
 
 def downgrade() -> None:
-    op.drop_constraint("uq_document_public_id", "document", schema="document")
-    op.drop_column("document", "public_id", schema="document")
+    op.drop_constraint("uq_document_public_id", "map_document_token", schema="document")
+    op.drop_column("map_document_token", "public_id", schema="document")
