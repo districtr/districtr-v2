adbce244e0ea7869aaac3f1c569cd9ad689c0540
nofurtherinformation <dhalpern@uchicago.edu>
2025-07-25 10:40:58 -0500

    prettier

--- app/src/app/components/Toolbar/SaveShareModal/MapDetailsSection.tsx
+++ app/src/app/components/Toolbar/SaveShareModal/MapDetailsSection.tsx
@@ -2,15 +2,15 @@ import {DocumentMetadata} from '@/app/utils/api/apiHandlers/types';
 import {Box, Button, Flex, Grid, Heading, Text, TextArea, TextField} from '@radix-ui/themes';
 import {MapStatusButtons} from '../../Topbar/MapStatus';
 import * as Accordion from '@radix-ui/react-accordion';
-import { useState } from 'react';
-import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import {useState} from 'react';
+import {ChevronDownIcon, ChevronUpIcon} from '@radix-ui/react-icons';
 
 export const MapDetailsSection: React.FC<{
   mapMetadata: DocumentMetadata;
   onChange: (updates: Partial<DocumentMetadata>) => any;
   isEditing: boolean;
 }> = ({mapMetadata, onChange, isEditing}) => {
-  const [accordionOpen, setAccordionOpen] = useState<string>("");
+  const [accordionOpen, setAccordionOpen] = useState<string>('');
   return (
     <Flex direction="column" gap="2">
       <Heading as="h3" size="5">
@@ -39,50 +39,54 @@ export const MapDetailsSection: React.FC<{
           />
         </Flex>
       </Box>
-      <Accordion.Root type="single" collapsible value={accordionOpen} onValueChange={value => setAccordionOpen(value)} >
+      <Accordion.Root
+        type="single"
+        collapsible
+        value={accordionOpen}
+        onValueChange={value => setAccordionOpen(value)}
+      >
         <Accordion.Item value="group">
-        <Accordion.AccordionTrigger className="AccordionTrigger w-full mt-2">
-          <Button className="w-full" variant="ghost">
-            <Flex direction="row" gap="2" align="center" justify="start" className="w-full">
-            <Text>Advanced</Text>
-            {accordionOpen === 'group' ? <ChevronUpIcon /> : <ChevronDownIcon />}
-            </Flex>
-
-          </Button>
-        </Accordion.AccordionTrigger>
-        <Accordion.AccordionContent className="AccordionContent">
-        <Grid columns="2" gap="2">
-          <Box>
-            <Text>Group</Text>
-            <TextField.Root
-              className="flex-1 w-full"
-              value={mapMetadata.group ?? ''}
-              placeholder="Group"
-              disabled={!isEditing}
-              onChange={e => onChange({group: e.target.value})}
-            />
-          </Box>
-          <Box>
-            <Text>Tags (coming soon)</Text>
-            <TextField.Root
-              className="flex-1 w-full"
-              value={mapMetadata.tags ?? ''}
-              placeholder="Tags"
-              disabled={true}
-            />
-          </Box>
-        </Grid>
-        <Box>
-          <Text>Comments</Text>
-          <TextArea
-            className="flex-1 w-full"
-            value={mapMetadata.description ?? ''}
-            placeholder="Comments or description"
-            onChange={e => onChange({description: e.target.value})}
-            disabled={!isEditing}
-          />
-        </Box>
-        </Accordion.AccordionContent>
+          <Accordion.AccordionTrigger className="AccordionTrigger w-full mt-2">
+            <Button className="w-full" variant="ghost">
+              <Flex direction="row" gap="2" align="center" justify="start" className="w-full">
+                <Text>Advanced</Text>
+                {accordionOpen === 'group' ? <ChevronUpIcon /> : <ChevronDownIcon />}
+              </Flex>
+            </Button>
+          </Accordion.AccordionTrigger>
+          <Accordion.AccordionContent className="AccordionContent">
+            <Grid columns="2" gap="2">
+              <Box>
+                <Text>Group</Text>
+                <TextField.Root
+                  className="flex-1 w-full"
+                  value={mapMetadata.group ?? ''}
+                  placeholder="Group"
+                  disabled={!isEditing}
+                  onChange={e => onChange({group: e.target.value})}
+                />
+              </Box>
+              <Box>
+                <Text>Tags (coming soon)</Text>
+                <TextField.Root
+                  className="flex-1 w-full"
+                  value={mapMetadata.tags ?? ''}
+                  placeholder="Tags"
+                  disabled={true}
+                />
+              </Box>
+            </Grid>
+            <Box>
+              <Text>Comments</Text>
+              <TextArea
+                className="flex-1 w-full"
+                value={mapMetadata.description ?? ''}
+                placeholder="Comments or description"
+                onChange={e => onChange({description: e.target.value})}
+                disabled={!isEditing}
+              />
+            </Box>
+          </Accordion.AccordionContent>
         </Accordion.Item>
       </Accordion.Root>
     </Flex>
