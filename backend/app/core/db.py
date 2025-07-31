09e044b1db59e063c450539e7f8a620c84999e1d
nofurtherinformation <dhalpern@uchicago.edu>
2025-07-31 09:57:49 -0500

    Revert "make load session for heavy, single threaded loads"
    This reverts commit dffb495a7ce3bdf0dd9c2ec2631191d2113d6e66.

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -9,26 +9,9 @@ engine = create_engine(
     pool_recycle=3600,
 )
 
-load_engine = create_engine(
-    str(settings.SQLALCHEMY_DATABASE_URI),
-    echo=settings.ECHO_DB,
-    pool_pre_ping=True,
-    pool_recycle=0,
-    pool_size=1,
-    timeout=3600,
-)
-
 
 def get_session():
     with Session(engine) as session:
-        try:
-            yield session
-        finally:
-            session.close()
-
-
-def get_load_session():
-    with Session(load_engine) as session:
         try:
             yield session
         finally:
