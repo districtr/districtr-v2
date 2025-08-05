0bdd20e31d3aab61f8721e11a5545447feaca5e0
nofurtherinformation <dhalpern@uchicago.edu>
2025-08-05 13:44:24 -0500

    Isolate content header component

--- app/src/app/components/Static/ContentSection.tsx
+++ app/src/app/components/Static/ContentSection.tsx
@@ -1,5 +1,6 @@
-import {Box, Flex, Heading} from '@radix-ui/themes';
+import {Box, Flex} from '@radix-ui/themes';
 import React from 'react';
+import { ContentHeader } from './ContentHeader';
 
 export const ContentSection: React.FC<{
   title: string;
@@ -9,9 +10,7 @@ export const ContentSection: React.FC<{
   return (
     <Box>
       <Flex direction="row" align="center" justify="start" my="2">
-        <Heading as="h2" className="font-bold p-4 bg-gray-100" size="6">
-          {title}
-        </Heading>
+        <ContentHeader title={title} />
         {!!flavorImage && flavorImage}
       </Flex>
       {children}
