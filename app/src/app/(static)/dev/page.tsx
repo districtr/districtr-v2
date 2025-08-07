8b52eee999b90b945a9b9532f60f13ec4d01a8be
nofurtherinformation <dhalpern@uchicago.edu>
2025-08-07 15:27:34 -0500

    Add env to prod and remove from log

--- app/src/app/(static)/dev/page.tsx
+++ app/src/app/(static)/dev/page.tsx
@@ -1,8 +1,6 @@
 import {CommentSubmissionForm} from '@/app/components/Forms/CommentSubmissionForm';
 
 export default function DevPage() {
-  // DEBUG ENV
-  console.log('!!', process.env);
   return (
     <CommentSubmissionForm
       mandatoryTags={['ca-redistricting-2025']}
