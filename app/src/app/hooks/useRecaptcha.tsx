34fae06cd9f65b39cbd7662834f0054fd773b93a
nofurtherinformation <dhalpern@uchicago.edu>
2025-08-07 14:22:14 -0500

    Try dynamic

--- app/src/app/hooks/useRecaptcha.tsx
+++ app/src/app/hooks/useRecaptcha.tsx
@@ -2,12 +2,13 @@
 import {useState} from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import {RECAPTCHA_SITE_KEY} from '../utils/api/constants';
+import { Text } from '@radix-ui/themes';
 
 export const useRecaptcha = () => {
   const [recaptchaToken, setRecaptchaToken] = useState<string | null>(null);
   const RecaptchaComponent = RECAPTCHA_SITE_KEY ? (
     <ReCAPTCHA sitekey={RECAPTCHA_SITE_KEY} onChange={value => setRecaptchaToken(value)} />
-  ) : null;
+  ) : <Text color="red">Error: Recaptcha is disabled</Text>;
   return {
     RecaptchaComponent,
     recaptchaToken,
