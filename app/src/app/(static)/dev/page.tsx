19fc9e35f51bf067162f11ed83c94f012c9f0136
nofurtherinformation <dhalpern@uchicago.edu>
2025-08-07 14:59:08 -0500

    Update page.tsx

--- app/src/app/(static)/dev/page.tsx
+++ app/src/app/(static)/dev/page.tsx
@@ -1,6 +1,8 @@
 import {CommentSubmissionForm} from '@/app/components/Forms/CommentSubmissionForm';
 
 export default function DevPage() {
+  // DEBUG ENV
+  console.log('!!', process.env);
   return (
     <CommentSubmissionForm
       mandatoryTags={['ca-redistricting-2025']}
