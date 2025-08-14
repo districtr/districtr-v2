21427c7ea362053eeed10a8b61addb3cceda7aeb
Nick Doiron <ndoiron@mapmeld.com>
2025-08-14 14:28:46 -0500

    component-ize

--- app/src/app/(static)/commentdev/[slug]/page.tsx
+++ app/src/app/(static)/commentdev/[slug]/page.tsx
@@ -1,8 +1,7 @@
-import {Box, Flex, Grid, Heading, IconButton, Link, Text} from '@radix-ui/themes';
+import {Flex, Heading} from '@radix-ui/themes';
+import {CommentList} from '@/app/components/Forms/CommentList';
 import {ContentSection} from '@/app/components/Static/ContentSection';
 import {getDocumentComments} from '@/app/utils/api/apiHandlers/getComments';
-import { PersonIcon } from '@radix-ui/react-icons';
-import { formatDistanceToNow } from 'date-fns'
 
 export default async function DevPage({params}: {params: Promise<{slug: string}>}) {
   const [{slug}] = await Promise.all([params]);
@@ -14,33 +13,7 @@ export default async function DevPage({params}: {params: Promise<{slug: string}>
         Latest Comments
       </Heading>
       <ContentSection title="Comment Demo">
-        {comments.map((c, idx) => (
-          <Box key={idx} className="flex flex-col border border-zinc-200 rounded-lg p-4 shadow-sm bg-white">
-            <Flex align="center" gap="3" mb="2">
-              {/*<IconButton variant="ghost" size="3" aria-label="Commenter">
-                <PersonIcon className="w-5 h-5 text-zinc-600" />
-              </IconButton>*/}
-              <Heading size="4" className="text-districtrBlue">
-                {c.comment.title}
-              </Heading>
-            </Flex>
-            <Text className="mb-3 whitespace-pre-line">{(c.comment.comment + "\nLorem ipsum")}</Text>
-            <Flex wrap="wrap" gap="2">
-              {c.tags?.map((tag) => (
-                <Link
-                  href={`/api/comments/tagged/${tag}`}
-                  key={tag}
-                  className="px-2 py-1 text-sm rounded-full bg-purple-100 text-black"
-                >
-                  {tag}
-                </Link>
-              ))}
-            </Flex>
-            <Text className="mt-1 text-gray-400 text-xs text-right">
-              {formatDistanceToNow(c.comment.created_at)} ago
-            </Text>
-          </Box>
-        ))}
+        <CommentList comments={comments} />
       </ContentSection>
     </Flex>
   );
