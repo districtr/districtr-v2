15ddb760e4f7ae992f4d479883cfda42f5dff5b8
nofurtherinformation <dhalpern@uchicago.edu>
2026-01-13 14:58:50 -0600

    Lint and clean up

--- app/src/app/components/Cms/RichTextEditor/extensions/PlanGallery/PlanGallery.tsx
+++ app/src/app/components/Cms/RichTextEditor/extensions/PlanGallery/PlanGallery.tsx
@@ -1,11 +1,10 @@
 'use client';
-import React, {useState} from 'react';
+import React from 'react';
 import {Table} from '@radix-ui/themes';
 import {Gallery} from '@/app/components/Static/Gallery';
 import {getPlans} from '@/app/utils/api/apiHandlers/getPlans';
 import {MinPublicDocument} from '@utils/api/apiHandlers/types';
 import {PlanCard, PlanFlags, PlanTableRow} from './PlanGalleryRenderers';
-import { useQuery } from '@tanstack/react-query';
 
 export type PlanGalleryProps = {
   ids?: Array<number>;
@@ -27,26 +26,6 @@ export const PlanGallery: React.FC<PlanGalleryProps> = ({
   showListView = false,
   ...flags
 }: PlanGalleryProps) => {
-  const [page, setPage] = useState(0);
-  const [displayLimit, _setDisplayLimit] = useState(+limit);
-  const [view, setView] = useState<'grid' | 'list'>('grid');
-  const {data: plans, isLoading} = useQuery({
-    queryKey: ['plans', ids, tags, page],
-    queryFn: async () => {
-      const result = await getPlans({
-        ids,
-        tags,
-        limit: displayLimit,
-        offset: page * displayLimit,
-      });
-      if (!result.ok) {
-        throw new Error(result.error.detail);
-      }
-      return result.response;
-    },
-  });
-  const showPagination = paginate && plans && (plans.length === displayLimit || page > 0);
-  const noMaps = !isLoading && !plans?.length;
   return (
     <Gallery<MinPublicDocument, {ids?: number[]; tags?: string[]}, MinPublicDocument[] | null>
       title={title}
@@ -57,7 +36,9 @@ export const PlanGallery: React.FC<PlanGalleryProps> = ({
       filters={{ids, tags}}
       queryKey={['plans']}
       queryFunction={({filters, limit, offset}) =>
-        getPlans({ids: filters.ids, tags: filters.tags, limit, offset})
+        getPlans({ids: filters.ids, tags: filters.tags, limit, offset}).then(result =>
+          result?.ok ? result.response : null
+        )
       }
       selectItems={data => (data || []) as MinPublicDocument[]}
       gridRenderer={(plan, i) => <PlanCard key={i} plan={plan} {...flags} />}
