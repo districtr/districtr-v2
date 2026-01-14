e8d3fa09e363156a74b0d95adf2b89b9bdf69944
nofurtherinformation <dhalpern@uchicago.edu>
2026-01-14 15:14:37 -0600

    Clean up get comments function

--- app/src/app/utils/api/apiHandlers/getComments.ts
+++ app/src/app/utils/api/apiHandlers/getComments.ts
@@ -42,35 +42,11 @@ export interface CommentFilters {
  * Only returns comments that have passed all moderation gates.
  */
 export const getPublicComments = async (filters: CommentFilters) => {
-  const searchParams = new URLSearchParams();
-
-  if (filters.ids) {
-    filters.ids.forEach(id => {
-      searchParams.append('ids', id.toString());
-    });
-  } else if (filters.tags) {
-    filters.tags.forEach(tag => {
-      searchParams.append('tags', tag);
-    });
+  const queryParams: Record<string, string | number | boolean | (string | number)[]> = {};
+  for (const [key, value] of Object.entries(filters)) {
+    if (value) {
+      queryParams[key] = value;
+    }
   }
-
-  if (filters.place) {
-    searchParams.append('place', filters.place);
-  }
-  if (filters.state) {
-    searchParams.append('state', filters.state);
-  }
-  if (filters.zipCode) {
-    searchParams.append('zipCode', filters.zipCode);
-  }
-  if (filters.offset) {
-    searchParams.append('offset', filters.offset.toString());
-  }
-  if (filters.limit) {
-    searchParams.append('limit', filters.limit.toString());
-  }
-
-  const queryString = searchParams.toString();
-  const path = `comments/list${queryString ? `?${queryString}` : ''}`;
-  return await get<CommentListing[]>(path)({});
+  return await get<CommentListing[]>('comments/list')({queryParams});
 };
