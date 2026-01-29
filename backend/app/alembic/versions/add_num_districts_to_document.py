53cac30032900c198c49439aab21f19a281ef277
nofurtherinformation <dhalpern@uchicago.edu>
2026-01-29 08:59:11 -0600

    Update migration history

--- backend/app/alembic/versions/add_num_districts_to_document.py
+++ backend/app/alembic/versions/add_num_districts_to_document.py
@@ -1,7 +1,7 @@
 """add num_districts to document
 
 Revision ID: a1b2c3d4e5f6
-Revises: 0db008690d60
+Revises: 5600a4bfe3c4
 Create Date: 2026-01-26 00:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "a1b2c3d4e5f6"
-down_revision: Union[str, None] = "0db008690d60"
+down_revision: Union[str, None] = "5600a4bfe3c4"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
