0b7bba9dd463eb8e4176ee9a9467572d4f8963f1
nofurtherinformation <dhalpern@uchicago.edu>
2026-03-02 15:33:38 -0600

    Simplify PublicDistrictLayer conditional rendering

--- app/src/app/components/Map/PublicDistrictLayer.tsx
+++ app/src/app/components/Map/PublicDistrictLayer.tsx
@@ -1,10 +1,7 @@
-import {
-  LABELS_BREAK_LAYER_ID,
-  ZONE_ASSIGNMENT_STYLE,
-} from '@/app/constants/layers';
+import {ZONE_ASSIGNMENT_STYLE} from '@/app/constants/map/layerStyle';
+import {BLOCK_SOURCE_ID, LABELS_BREAK_LAYER_ID} from '@/app/constants/map/layerIds';
 import {useMapStore} from '@/app/store/mapStore';
-import {Layer, Source} from 'react-map-gl/dist/esm/exports-maplibre';
-import {BLOCK_SOURCE_ID} from '@/app/constants/layers';
+import {Layer, Source} from 'react-map-gl/maplibre';
 import {DemographicLayer} from './DemographicLayer';
 import {useChoroplethRenderer} from '@/app/hooks/useChoroplethRenderer';
 
@@ -16,57 +13,54 @@ export const PublicDistrictLayer = ({isDemographicMap}: {isDemographicMap?: bool
 
   return (
     <Source id={BLOCK_SOURCE_ID} type="geojson" data={getPublicMapData()} promoteId="zone">
-      {!isDemographicMap && (
-        <Layer
-          id={'public-districts-layer-line'}
-          beforeId={LABELS_BREAK_LAYER_ID}
-          type="line"
-          layout={{
-            visibility: 'visible',
-          }}
-          paint={{
-            'line-opacity': 0.8,
-            // 'line-color': '#aaaaaa', // Default color
-            'line-color': [
-              'interpolate',
-              ['exponential', 1.6],
-              ['zoom'],
-              6,
-              '#aaa',
-              9,
-              '#777',
-              14,
-              '#333',
-            ],
-            'line-width': [
-              'interpolate',
-              ['exponential', 1.6],
-              ['zoom'],
-              6,
-              lineWidth * 0.125,
-              9,
-              lineWidth * 0.35,
-              14,
-              lineWidth,
-            ],
-          }}
-        />
+      {isDemographicMap ? (
+        <DemographicLayer />
+      ) : (
+        <>
+          <Layer
+            id={'public-districts-layer-line'}
+            beforeId={LABELS_BREAK_LAYER_ID}
+            type="line"
+            layout={{
+              visibility: 'visible',
+            }}
+            paint={{
+              'line-opacity': 0.8,
+              'line-color': [
+                'interpolate',
+                ['exponential', 1.6],
+                ['zoom'],
+                6,
+                '#aaa',
+                9,
+                '#777',
+                14,
+                '#333',
+              ],
+              'line-width': [
+                'interpolate',
+                ['exponential', 1.6],
+                ['zoom'],
+                6,
+                lineWidth * 0.125,
+                9,
+                lineWidth * 0.35,
+                14,
+                lineWidth,
+              ],
+            }}
+          />
+          <Layer
+            id="public-districts-layer"
+            type="fill"
+            beforeId={LABELS_BREAK_LAYER_ID}
+            paint={{
+              'fill-opacity': 0.7,
+              'fill-color': ZONE_ASSIGNMENT_STYLE(colorScheme) || '#000000',
+            }}
+          />
+        </>
       )}
-      {!isDemographicMap && (
-        <Layer
-          id="public-districts-layer"
-          type="fill"
-          source="public-districts"
-          beforeId={LABELS_BREAK_LAYER_ID}
-          paint={{
-            'fill-opacity': 0.7,
-            'fill-color':
-              ZONE_ASSIGNMENT_STYLE(colorScheme, (i: number) => ['==', ['get', 'zone'], i + 1]) ||
-              '#000000',
-          }}
-        />
-      )}
-      {isDemographicMap && <DemographicLayer />}
     </Source>
   );
 };
