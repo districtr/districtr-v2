9bdc582bf4f7702be37473eb6d72e615b396edda
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2026-03-19 20:39:36 -0600

    Fix annoying landing page error message when you load into /map/edit

---
+++ app/src/app/(interactive)/map/edit/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MapPage from '@/app/components/MapPage/MapPage';
+
+export default function MapEditLandingPage() {
+  return <MapPage isEditing={true} mapId="" />;
+}
