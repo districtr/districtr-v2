b40704286bbdc98102fa5e6e928621848cb83a2c
Dylan <dhalpern@uchicago.edu>
2026-07-21 15:07:30 -0500

    Legacy flow

---
+++ app/src/app/not-found.tsx
@@ -0,0 +1,28 @@
+'use client';
+import {usePathname} from 'next/navigation';
+import {Box, Flex, Heading, Link, Text} from '@radix-ui/themes';
+import {Header} from '@components/Static/Header';
+import {Footer} from '@components/Static/Footer';
+
+export default function NotFound() {
+  const pathname = usePathname();
+  const legacyUrl = `https://districtr.org${pathname ?? ''}`;
+
+  return (
+    <Flex direction="column" className="min-h-[100vh]" justify="center">
+      <Header />
+      <Box className="w-full flex-grow p-4 pt-0 max-w-screen-lg mx-auto px-4 xl:px-0">
+        <Flex direction="column" gapY="4" align="center" py="9">
+          <Heading as="h1">Page not found</Heading>
+          <Text size="3" align="center">
+            You&apos;ve landed on Districtr 2.0, and this page doesn&apos;t exist here. Looking for
+            a page from the original Districtr? Try{' '}
+            <Link href={legacyUrl}>districtr.org{pathname}</Link>.
+          </Text>
+          <Link href="/">Back to the Districtr 2.0 home page</Link>
+        </Flex>
+      </Box>
+      <Footer />
+    </Flex>
+  );
+}
