63dfa09af4be5558c214ede4137694340e603781
Dylan <dhalpern@uchicago.edu>
2026-07-24 11:25:33 -0500

    Legacy flow (#645)

---
+++ app/src/app/constants/legacy.ts
@@ -0,0 +1,3 @@
+// Base URL of the original (classic) Districtr app.
+// Currently districtr.org; will move to legacy.districtr.org when v2 takes over the apex domain.
+export const LEGACY_DISTRICTR_URL = 'https://districtr.org';
